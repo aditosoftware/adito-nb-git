1da7c2e839df4d795e82a455c3a7afb82c4cfffe
m.kaspera <m.kaspera@adito.de>
2018-09-21 13:53:41 +0200

    added inherited comments, added constructor

--- src/main/java/de/adito/git/api/data/FileStatusImpl.java
+++ src/main/java/de/adito/git/api/data/FileStatusImpl.java
@@ -1,8 +1,8 @@
-package data;
+package de.adito.git.api.data;
 
 import org.eclipse.jgit.api.Status;
 import org.eclipse.jgit.lib.IndexDiff;
-import wrappers.EStageState;
+import de.adito.git.wrappers.EStageState;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,46 +15,94 @@ public class FileStatusImpl implements IFileStatus {
 
     private Status status;
 
+    FileStatusImpl(Status pStatus){
+        status = pStatus;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @return
+     */
     public boolean isClean() {
         return status.isClean();
     }
 
+    /**
+     * {@inheritDoc}
+     * @return
+     */
     public boolean hasUncommittedChanges() {
         return status.hasUncommittedChanges();
     }
 
+    /**
+     * {@inheritDoc}
+     * @return
+     */
     public Set<String> getAdded() {
         return status.getAdded();
     }
 
+    /**
+     * {@inheritDoc}
+     * @return
+     */
     public Set<String> getChanged() {
         return status.getChanged();
     }
 
+    /**
+     * {@inheritDoc}
+     * @return
+     */
     public Set<String> getRemoved() {
         return status.getRemoved();
     }
 
+    /**
+     * {@inheritDoc}
+     * @return
+     */
     public Set<String> getMissing() {
         return status.getMissing();
     }
 
+    /**
+     * {@inheritDoc}
+     * @return
+     */
     public Set<String> getModified() {
         return status.getModified();
     }
 
+    /**
+     * {@inheritDoc}
+     * @return
+     */
     public Set<String> getUntracked() {
         return status.getUntracked();
     }
 
+    /**
+     * {@inheritDoc}
+     * @return
+     */
     public Set<String> getUntrackedFolders() {
         return status.getUntrackedFolders();
     }
 
+    /**
+     * {@inheritDoc}
+     * @return
+     */
     public Set<String> getConflicting() {
         return status.getConflicting();
     }
 
+    /**
+     * {@inheritDoc}
+     * @return
+     */
     public Map<String, EStageState> getConflictingStageState() {
         Map<String, EStageState> conflictingStageState = new HashMap<String, EStageState>();
         Map<String, IndexDiff.StageState> jgitConflictingStageState = status.getConflictingStageState();
@@ -64,10 +112,18 @@ public class FileStatusImpl implements IFileStatus {
         return conflictingStageState;
     }
 
+    /**
+     * {@inheritDoc}
+     * @return
+     */
     public Set<String> getIgnoredNotInIndex() {
         return status.getIgnoredNotInIndex();
     }
 
+    /**
+     * {@inheritDoc}
+     * @return
+     */
     public Set<String> getUncommittedChanges() {
         return status.getUncommittedChanges();
     }
