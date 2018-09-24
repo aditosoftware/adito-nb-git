8623226c16228330d4ef3ead78af900d688562bd
m.kaspera <m.kaspera@adito.de>
2018-09-24 16:43:13 +0200

    added comments in interface

--- API/src/main/java/de/adito/git/api/IFileDiff.java
+++ API/src/main/java/de/adito/git/api/IFileDiff.java
@@ -10,16 +10,49 @@ import de.adito.git.api.data.FileChangeObj;
  */
 public interface IFileDiff {
 
+    /**
+     *
+     * @return the line from which an add/remove begins
+     */
     int getChangeStartLine();
 
+    /**
+     *
+     * @return the line at which the add/remove stops
+     */
     int getChangeEndLine();
 
+    /**
+     *
+     * @param side {@link EChangeSide} that tells if the older or later branch/commit/... should be inspected
+     * @return the identifier for the file/object on the specified side of the tree
+     */
+    String getId(EChangeSide side);
+
+    /**
+     *
+     * @return {@link EChangeType} that tells which kind of change happened (add/remove...)
+     */
     EChangeType getChangeType();
 
+    /**
+     *
+     * @param side {@link EChangeSide} that tells if the older or later branch/commit/... should be inspected
+     * @return {@link EFileType} which kind of file
+     */
     EFileType getFileType(EChangeSide side);
 
+    /**
+     *
+     * @param side {@link EChangeSide} that tells if the older or later branch/commit/... should be inspected
+     * @return the path from root to the file
+     */
     String getFilePath(EChangeSide side);
 
+    /**
+     *
+     * @return {@link FileChangeObj} that contains a list detailing the changes in each changed line
+     */
     FileChangeObj getFileChanges();
 
 }
