18f54b1252803527566844d7f0b931c859e5f41f
m.kaspera <m.kaspera@adito.de>
2018-09-27 16:22:02 +0200

    resizing/general size behaviour changes for the table

--- gui/src/main/java/de/adito/git/StatusWindow.java
+++ gui/src/main/java/de/adito/git/StatusWindow.java
@@ -3,8 +3,12 @@ package de.adito.git;
 import de.adito.git.api.IFileStatus;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
+ * class to display the results of the status command to git (i.e. lists all changes made to the
+ * local filesystem in comparison to HEAD)
+ *
  * @author m.kaspera 27.09.2018
  */
 public class StatusWindow extends JPanel {
@@ -17,7 +21,13 @@ public class StatusWindow extends JPanel {
     }
 
     private void _initGui(){
-        add(new JTable(new StatusTableModel(status)));
+        setLayout(new BorderLayout());
+        JTable statusTable = new JTable(new StatusTableModel(status));
+        statusTable.getColumnModel().getColumn(0).setMinWidth(150);
+        statusTable.getColumnModel().getColumn(1).setMinWidth(250);
+        statusTable.getColumnModel().getColumn(2).setMinWidth(50);
+        statusTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+        add(statusTable, BorderLayout.CENTER);
     }
 
 }
