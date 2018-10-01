c20dc88faa37146347942a5388421809414f4191
A.Arnold <a.arnold@adito.de>
2018-10-01 15:39:22 +0200

    Implement new Exceptions

--- api/src/main/java/de/adito/git/api/IRepository.java
+++ api/src/main/java/de/adito/git/api/IRepository.java
@@ -19,32 +19,33 @@ public interface IRepository {
      * @param addList List of files to add to staging
      * @return {@code true} if the operation was successful, {@code false} otherwise
      */
-    List add(List<File> addList);
+    List add(List<File> addList) throws Exception;
 
     /**
      * @param message String with the commit message entered by the user
      * @return the ID of the commit as String
      */
-    String commit(@NotNull String message);
+    String commit(@NotNull String message) throws Exception;
 
     /**
      * Pushes the changes made to HEAD onto the selected branch/remote
+     *
      * @return {@code true} if the operation was successful, {@code false} otherwise
      */
-    boolean push();
+    boolean push() throws Exception;
 
     /**
      * @param targetId the id/url of the branch/remote to pull from
      * @return {@code true} if the pull was successful, {@code false otherwise}
      */
-    boolean pull(@NotNull String targetId);
+    boolean pull(@NotNull String targetId) throws Exception;
 
     /**
      * @param original  the older ICommit
      * @param compareTo the later ICommit
      * @return IFileDiff that contains the differences between the two files
      */
-    @NotNull List<IFileDiff> diff(@NotNull ICommit original, @NotNull ICommit compareTo) throws IOException;
+    @NotNull List<IFileDiff> diff(@NotNull ICommit original, @NotNull ICommit compareTo) throws Exception;
 
     /**
      * @param url  the url from which to pull from, as String
@@ -74,19 +75,18 @@ public interface IRepository {
      * @param checkout   {@code true} if the branch should be automatically checked out after it was created
      * @return the identifier for the branch as String
      */
-    @NotNull String createBranch(@NotNull String branchName, boolean checkout);
+    @NotNull String createBranch(@NotNull String branchName, boolean checkout) throws Exception;
 
     /**
      * @param branchName the name of the branch to delete
      */
-    void deleteBranch(@NotNull String branchName);
+    void deleteBranch(@NotNull String branchName) throws Exception;
 
     /**
-     *
      * @param branchName String with identifier of the branch to checkout
      * @return {@code true} if the operation was successful, {@code false} otherwise
      */
-    boolean checkout(@NotNull String branchName);
+    boolean checkout(@NotNull String branchName) throws Exception;
 
     /**
      * @param sourceName String with identifier of source branch
@@ -100,35 +100,37 @@ public interface IRepository {
      * @param targetName String with identifier of target branch
      * @return {@code true} if the operation was successful, {@code false} otherwise
      */
-    public boolean merge(@NotNull String sourceName, @NotNull String targetName, @NotNull String commitMessage);
+    public boolean merge(@NotNull String sourceName, @NotNull String targetName, @NotNull String commitMessage) throws Exception;
 
     /**
      * @param identifier String with identifier of the commit
      * @return ICommit describing the commit
      */
-    ICommit getCommit(String identifier);
+    ICommit getCommit(String identifier) throws Exception;
 
     /**
      * @param sourceBranch IBranch for which all commits should be retrieved
      * @return List with all ICommits in the sourceBranch
      */
-    List<ICommit> getCommits(IBranch sourceBranch) throws IOException;
+    List<ICommit> getCommits(IBranch sourceBranch) throws Exception;
 
     /**
      * @param forFile File for which all commits should be retrieved
      * @return List with ICommits that contains all commits that affected the file
      */
-    List<ICommit> getCommits(File forFile) throws IOException;
+    List<ICommit> getCommits(File forFile) throws Exception;
+
+    List<ICommit> getAllCommits() throws Exception;
 
     /**
      * @param identifier String with identifier of the targeted branch
      * @return the IBranch for the given identifier
      */
-    IBranch getBranch(String identifier);
+    IBranch getBranch(String identifier) throws Exception;
 
     /**
      * @return List of all IBranches in the repository
      */
-    List<IBranch> getBranches();
+    List<IBranch> getBranches() throws Exception;
 
 }
