6e2f15db26d6803c505d5980c61f6be3d38b8e93
m.kaspera <m.kaspera@adito.de>
2018-10-10 10:04:19 +0200

    StatusWindow is injected

--- nbm/src/main/java/de/adito/git/nbm/VCSTopComponent.java
+++ nbm/src/main/java/de/adito/git/nbm/VCSTopComponent.java
@@ -2,12 +2,9 @@ package de.adito.git.nbm;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import de.adito.git.nbm.Guice.AditoNbmModule;
-import de.adito.git.api.IRepository;
-import de.adito.git.gui.IDialogDisplayer;
-import de.adito.git.gui.RepositoryProvider;
 import de.adito.git.gui.StatusWindow;
 import de.adito.git.gui.guice.AditoGitModule;
+import de.adito.git.nbm.Guice.AditoNbmModule;
 import org.openide.windows.TopComponent;
 
 import javax.swing.*;
@@ -17,8 +14,7 @@ import java.awt.*;
  * @author m.kaspera 28.09.2018
  */
 @TopComponent.Description(preferredID = "VCSTopComponent",
-    iconBase = "",
-    persistenceType = TopComponent.PERSISTENCE_ONLY_OPENED)
+        persistenceType = TopComponent.PERSISTENCE_ONLY_OPENED)
 @TopComponent.Registration(mode = "output", openAtStartup = true, position = 1000)
 public class VCSTopComponent extends TopComponent {
 
@@ -26,11 +22,9 @@ public class VCSTopComponent extends TopComponent {
         setLayout(new BorderLayout());
         Injector injector;
         injector = Guice.createInjector(new AditoGitModule(), new AditoNbmModule());
-        IRepository git = injector.getInstance(RepositoryProvider.class).getRepositoryImpl();
 
-        StatusWindow statusWindow = new StatusWindow(git.status(), injector.getInstance(IDialogDisplayer.class), git);
         add(new JLabel("test success"), BorderLayout.NORTH);
-        add(statusWindow, BorderLayout.CENTER);
+        add(injector.getInstance(StatusWindow.class), BorderLayout.CENTER);
     }
 
     @Override
