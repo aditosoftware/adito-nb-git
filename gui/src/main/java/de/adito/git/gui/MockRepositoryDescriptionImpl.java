71f2e077c006aad059e6dfd2251fa4375aadad9b
m.kaspera <m.kaspera@adito.de>
2018-10-15 08:01:05 +0200

    RepositoryDescription can now exist more than once, manually passed

--- gui/src/main/java/de/adito/git/gui/MockRepositoryDescriptionImpl.java
+++ gui/src/main/java/de/adito/git/gui/MockRepositoryDescriptionImpl.java
@@ -2,8 +2,6 @@ package de.adito.git.gui;
 
 import de.adito.git.api.data.IRepositoryDescription;
 
-import java.util.ResourceBundle;
-
 /**
  * Mock repository implementation for testing purposes
  *
@@ -11,15 +9,18 @@ import java.util.ResourceBundle;
  */
 public class MockRepositoryDescriptionImpl implements IRepositoryDescription {
 
-    public MockRepositoryDescriptionImpl(){
+    private String path;
 
+    public MockRepositoryDescriptionImpl(String pPath){
+        path = pPath;
     }
 
     @Override
     public String getPath() {
         //return "C:\\Entwicklungszweige\\adito-nb-git\\.git";
 //        return ResourceBundle.getBundle(this.getClass().getPackage().getName() + ".Bundle").getString("VCS_Directory");
-        return System.getProperty("vcsDirectory");
+        //return System.getProperty("vcsDirectory");
+        return path;
     }
 
     @Override
