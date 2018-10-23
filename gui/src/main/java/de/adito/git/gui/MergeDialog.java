7824d9ea361e391a4758656c02a16a4dd5a84311
m.kaspera <m.kaspera@adito.de>
2018-10-23 15:31:54 +0200

    Added a row of Buttons that accepts changed chunks
    Still open:
     - Discard change Buttons
     - Middle textPane is editable by the user
     - Accept yours/theirs buttons

--- gui/src/main/java/de/adito/git/gui/MergeDialog.java
+++ gui/src/main/java/de/adito/git/gui/MergeDialog.java
@@ -1,12 +1,15 @@
 package de.adito.git.gui;
 
+import de.adito.git.api.data.EChangeType;
 import de.adito.git.api.data.IFileChangeChunk;
 import de.adito.git.api.data.IMergeDiff;
 import io.reactivex.disposables.Disposable;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * Dialog/Panel for displaying the merge-conflicts
@@ -21,6 +24,8 @@ public class MergeDialog extends JPanel implements IDiscardable {
     private final JTextPane currentBranchVersionPane = new JTextPane();
     private final JTextPane forkPointVersionPane = new JTextPane();
     private final JTextPane mergeBranchVersionPane = new JTextPane();
+    private final JPanel leftButtonPanel = new JPanel(null);
+    private final JPanel rightButtonPanel = new JPanel(null);
     private Disposable currentBranchDisposable;
     private Disposable mergeBranchDisposeable;
 
@@ -36,9 +41,13 @@ public class MergeDialog extends JPanel implements IDiscardable {
         JPanel currentBranchPanel = new JPanel(new BorderLayout());
         JPanel forkPointPanel = new JPanel(new BorderLayout());
         JPanel mergeBranchPanel = new JPanel(new BorderLayout());
+        leftButtonPanel.setPreferredSize(new Dimension(30, 1200));
+        rightButtonPanel.setPreferredSize(new Dimension(30, 1200));
         currentBranchPanel.add(currentBranchVersionPane, BorderLayout.CENTER);
-        forkPointPanel.add(forkPointVersionPane);
-        mergeBranchPanel.add(mergeBranchVersionPane);
+        currentBranchPanel.add(leftButtonPanel, BorderLayout.EAST);
+        forkPointPanel.add(forkPointVersionPane, BorderLayout.CENTER);
+        mergeBranchPanel.add(mergeBranchVersionPane, BorderLayout.CENTER);
+        mergeBranchPanel.add(rightButtonPanel, BorderLayout.WEST);
         JScrollPane currentBranchScrollPane = new JScrollPane(currentBranchPanel);
         JScrollPane forkPointScrollPane = new JScrollPane(forkPointPanel);
         JScrollPane mergeBranchScrollPane = new JScrollPane(mergeBranchPanel);
@@ -61,7 +70,11 @@ public class MergeDialog extends JPanel implements IDiscardable {
                 // reset the text, else the text will just be appended to the textPane after each accepted change
                currentBranchVersionPane.setText("");
                forkPointVersionPane.setText("");
+               _clearPanel(leftButtonPanel);
                for(IFileChangeChunk changeChunk: changeChunks){
+                   if(changeChunk.getChangeType() != EChangeType.SAME) {
+                       _insertButtonForChunk(leftButtonPanel, ">", changeChunk, mergeDiff::insertBaseSideChunk, currentBranchVersionPane.getFontMetrics(currentBranchVersionPane.getFont()).getHeight());
+                   }
                    TextHighlightUtil.insertText(currentBranchVersionPane.getStyledDocument(), changeChunk.getBLines(), changeChunk.getChangeType());
                    TextHighlightUtil.insertText(forkPointVersionPane.getStyledDocument(), changeChunk.getALines(), Color.WHITE);
                }
@@ -70,7 +83,11 @@ public class MergeDialog extends JPanel implements IDiscardable {
             mergeBranchDisposeable = mergeDiff.getMergeSideDiff().getFileChanges().getChangeChunks().subscribe(changeChunks -> {
                 // reset the text, else the text will just be appended to the textPane after each accepted change
                 mergeBranchVersionPane.setText("");
+                _clearPanel(rightButtonPanel);
                 for (IFileChangeChunk changeChunk : changeChunks) {
+                    if(changeChunk.getChangeType() != EChangeType.SAME) {
+                        _insertButtonForChunk(rightButtonPanel, "<", changeChunk, mergeDiff::insertMergeSideChunk, currentBranchVersionPane.getFontMetrics(currentBranchVersionPane.getFont()).getHeight());
+                    }
                     TextHighlightUtil.insertText(mergeBranchVersionPane.getStyledDocument(), changeChunk.getBLines(), changeChunk.getChangeType());
                 }
             });
@@ -85,5 +102,25 @@ public class MergeDialog extends JPanel implements IDiscardable {
         if(mergeBranchDisposeable != null && !mergeBranchDisposeable.isDisposed()){
             mergeBranchDisposeable.dispose();
         }
+    }
+
+    private void _insertButtonForChunk(JPanel drawArea, String text, IFileChangeChunk changeChunk, Consumer<IFileChangeChunk> onClick, int lineHeight){
+        JButton acceptChangeButton = new JButton(text);
+        drawArea.add(acceptChangeButton);
+        acceptChangeButton.setBounds(0, (changeChunk.getBStart() + (changeChunk.getBEnd() - changeChunk.getBStart())/2) * lineHeight, 20, 20);
+        acceptChangeButton.addActionListener(e -> onClick.accept(changeChunk));
+    }
+
+    private void _clearPanel(JPanel panel){
+        for(Component component: panel.getComponents()){
+            if(component instanceof JButton){
+                for(ActionListener actionListener: ((JButton) component).getActionListeners()){
+                    ((JButton) component).removeActionListener(actionListener);
+                }
+            }
+        }
+        panel.removeAll();
+        panel.validate();
+        panel.repaint();
     }
 }
