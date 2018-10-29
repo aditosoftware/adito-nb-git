ce6446a51e8788c54173f24ac1a74829736b4c94
m.kaspera <m.kaspera@adito.de>
2018-10-29 14:59:50 +0100

    Implementations of the provider interfaces of Actions/Dialogs/Windows

---
+++ gui/src/main/java/de/adito/git/gui/actions/ActionProvider.java
@@ -0,0 +1,82 @@
+package de.adito.git.gui.actions;
+
+import com.google.inject.Inject;
+import de.adito.git.api.IRepository;
+import de.adito.git.api.data.IBranch;
+import de.adito.git.api.data.IFileChangeType;
+import io.reactivex.Observable;
+
+import java.util.List;
+
+/**
+ * @author m.kaspera 26.10.2018
+ */
+public class ActionProvider implements IActionProvider {
+
+    private final IActionFactory actionFactory;
+
+    @Inject
+    ActionProvider(IActionFactory actionFactory) {
+        this.actionFactory = actionFactory;
+    }
+
+    public MergeAction getMergeAction(Observable<IRepository> pRepository, String pCurrentBranch, String pTargetBranch) {
+        return actionFactory.createMergeAction(pRepository, pCurrentBranch, pTargetBranch);
+    }
+
+    @Override
+    public CommitAction getCommitAction(Observable<IRepository> pRepository, Observable<List<IFileChangeType>> pSelectedFilesObservable) {
+        return actionFactory.createCommitAction(pRepository, pSelectedFilesObservable);
+    }
+
+    @Override
+    public DiffAction getDiffAction(Observable<IRepository> pRepository, Observable<List<IFileChangeType>> pSelectedFilesObservable) {
+        return actionFactory.createDiffAction(pRepository, pSelectedFilesObservable);
+    }
+
+    @Override
+    public IgnoreAction getIgnoreAction(Observable<IRepository> pRepository, Observable<List<IFileChangeType>> pSelectedFilesObservable) {
+        return actionFactory.createIgnoreAction(pRepository, pSelectedFilesObservable);
+    }
+
+    @Override
+    public AddAction getAddAction(Observable<IRepository> pRepository, Observable<List<IFileChangeType>> pSelectedFilesObservable) {
+        return actionFactory.createAddAction(pRepository, pSelectedFilesObservable);
+    }
+
+    @Override
+    public ExcludeAction getExcludeAction(Observable<IRepository> pRepository, Observable<List<IFileChangeType>> pSelectedFilesObservable) {
+        return actionFactory.createExcludeAction(pRepository, pSelectedFilesObservable);
+    }
+
+    @Override
+    public CheckoutAction getCheckoutAction(Observable<IRepository> pRepository, Observable<List<IBranch>> pBranchList) {
+        return actionFactory.createCheckoutAction(pRepository, pBranchList);
+    }
+
+    @Override
+    public NewBranchAction getNewBranchAction(Observable<IRepository> pRepository) {
+        return actionFactory.createNewBranchAction(pRepository);
+    }
+
+    @Override
+    public PullAction getPullAction(Observable<IRepository> pRepository, String pTargetId) {
+        return actionFactory.createPullAction(pRepository, pTargetId);
+    }
+
+    @Override
+    public PushAction getPushAction(Observable<IRepository> pRepository) {
+        return actionFactory.createPushAction(pRepository);
+    }
+
+    @Override
+    public ShowAllBranchesAction getShowAllBranchesAction(Observable<IRepository> pRepository) {
+        return actionFactory.createShowAllBranchesAction(pRepository);
+    }
+
+    @Override
+    public ShowAllCommitsAction getShowAllCommitsAction(Observable<IRepository> pRepository, Observable<List<IBranch>> pBranches) {
+        return actionFactory.createShowAllCommitsAction(pRepository, pBranches);
+    }
+
+}
