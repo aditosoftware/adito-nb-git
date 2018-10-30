bf11dfa865d38bfd833e41560cdcb02e859ad4a1
m.kaspera <m.kaspera@adito.de>
2018-10-30 13:04:05 +0100

    Added NotNull annotations where applicable

--- gui/src/main/java/de/adito/git/gui/guice/GuiceUtil.java
+++ gui/src/main/java/de/adito/git/gui/guice/GuiceUtil.java
@@ -4,6 +4,7 @@ import com.google.inject.Binding;
 import com.google.inject.Key;
 import com.google.inject.Module;
 import com.google.inject.spi.Elements;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,7 +21,8 @@ public class GuiceUtil {
      * @param toRemove the binding that should be removed/filtered from the module
      * @return a new Module all the bindings/factories/stuff from the passed Module, minus the filtered bindings
      */
-    public static Module filterModule(Module toFilter, Key<?> toRemove) {
+    @NotNull
+    public static Module filterModule(@NotNull Module toFilter, @NotNull Key<?> toRemove) {
         return Elements.getModule(Elements.getElements(toFilter).stream().filter(element -> {
             if (element instanceof Binding)
                 return !((Binding) element).getKey().equals(toRemove);
@@ -35,7 +37,8 @@ public class GuiceUtil {
      * @param toRemove the bindings that should be removed/filtered from the module
      * @return a new Module all the bindings/factories/stuff from the passed Module, minus the filtered bindings
      */
-    public static Module filterModule(Module toFilter, List<Key<?>> toRemove) {
+    @NotNull
+    public static Module filterModule(@NotNull Module toFilter, @NotNull List<Key<?>> toRemove) {
         return Elements.getModule(Elements.getElements(toFilter).stream().filter(element -> {
             if (element instanceof Binding)
                 return toRemove.stream().noneMatch(key -> key.equals(((Binding) element).getKey()));
