678bf17e0a763c4eda432af6eafa07a99b2ee8ef
m.kaspera <m.kaspera@adito.de>
2018-10-31 09:55:17 +0100

    Implemented RevertWorkDirAction, basics for ResetAction

--- gui/src/main/java/de/adito/git/gui/actions/IActionFactory.java
+++ gui/src/main/java/de/adito/git/gui/actions/IActionFactory.java
@@ -36,4 +36,6 @@ interface IActionFactory {
     ShowAllBranchesAction createShowAllBranchesAction(Observable<IRepository> pRepository);
 
     ShowAllCommitsAction createShowAllCommitsAction(Observable<IRepository> pRepository, Observable<List<IBranch>> pBranches);
+
+    RevertWorkDirAction createRevertWorkDirAction(Observable<IRepository> pRepository, Observable<List<IFileChangeType>> pSelectedFilesObservable);
 }
---
+++ gui/src/main/java/de/adito/git/gui/actions/RevertWorkDirAction.java
@@ -0,0 +1,43 @@
+package de.adito.git.gui.actions;
+
+import com.google.inject.assistedinject.Assisted;
+import de.adito.git.api.IRepository;
+import de.adito.git.api.data.EChangeType;
+import de.adito.git.api.data.IFileChangeType;
+import io.reactivex.Observable;
+
+import java.awt.event.ActionEvent;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author m.kaspera 31.10.2018
+ */
+class RevertWorkDirAction extends AbstractTableAction {
+
+    private final IRepository repository;
+    private final Observable<List<IFileChangeType>> selectedFilesObservable;
+
+    RevertWorkDirAction(@Assisted Observable<IRepository> pRepository,
+                        @Assisted Observable<List<IFileChangeType>> pSelectedFilesObservable) {
+        super("Revert");
+        repository = pRepository.blockingFirst();
+        selectedFilesObservable = pSelectedFilesObservable;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            repository.revertWorkDir(selectedFilesObservable.blockingFirst().stream().map(IFileChangeType::getFile).collect(Collectors.toList()));
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    @Override
+    protected boolean isEnabled0() {
+        return selectedFilesObservable.blockingFirst()
+                .stream()
+                .noneMatch(fileChangeType -> fileChangeType.getChangeType().equals(EChangeType.SAME));
+    }
+}
