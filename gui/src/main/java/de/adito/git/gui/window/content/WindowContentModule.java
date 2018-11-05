e2c9550d893465fcb39b4f49d911b63d08889e1f
m.kaspera <m.kaspera@adito.de>
2018-11-05 13:24:11 +0100

    Last class comment for Modules

--- gui/src/main/java/de/adito/git/gui/window/content/WindowContentModule.java
+++ gui/src/main/java/de/adito/git/gui/window/content/WindowContentModule.java
@@ -4,6 +4,9 @@ import com.google.inject.AbstractModule;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 
 /**
+ *
+ * Guice module that defines the bindings for the window.content package
+ *
  * @author m.kaspera 29.10.2018
  */
 public class WindowContentModule extends AbstractModule {
