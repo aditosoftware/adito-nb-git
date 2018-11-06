b0727f97b2c724ee03353c76407a68076b7c571d
m.kaspera <m.kaspera@adito.de>
2018-11-06 15:11:54 +0100

    Colors in tables are now retrieved by UIManager keys in the EChangeType

--- api/src/main/java/de/adito/git/api/data/EChangeType.java
+++ api/src/main/java/de/adito/git/api/data/EChangeType.java
@@ -1,38 +1,58 @@
 package de.adito.git.api.data;
 
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Enum that describes the type of change that happened to a file
  *
  * @author m.kaspera 24.09.2018
  */
 public enum EChangeType {
+
     /** Add a new file to the project, already in the index */
-    ADD,
+    ADD("nb.versioning.added.color"),
 
     /** new file in the project, file is not yet staged */
-    NEW,
+    NEW("nb.versioning.added.color"),
 
     /** Modify an existing file in the project (content and/or mode), changes are staged */
-    CHANGED,
+    CHANGED("nb.versioning.modified.color"),
 
     /** Modify an existing file in the project (content and/or mode), changes not in the index */
-    MODIFY,
+    MODIFY("nb.versioning.modified.color"),
 
     /** Delete an existing file from the project */
-    DELETE,
+    DELETE("nb.versioning.deleted.color"),
 
     /** File is in index but not on the local disk */
-    MISSING,
+    MISSING("nb.versioning.deleted.color"),
 
     /** Rename an existing file to a new location */
-    RENAME,
+    RENAME("nb.versioning.modified.color"),
 
     /** Copy an existing file to a new location, keeping the original */
-    COPY,
+    COPY("nb.versioning.added.color"),
 
     /** file is in a conflicting state towards (e.g what you get if you modify file that was modified by someone else in the meantime) */
-    CONFLICTING,
+    CONFLICTING("nb.versioning.conflicted.color"),
 
     /** Stayed the same, only used for LineChanges */
-    SAME
+    SAME(null);
+
+    Color color;
+
+    EChangeType(String pUIManagerKey) {
+        if (pUIManagerKey != null)
+            color = UIManager.getColor(pUIManagerKey);
+        else
+            color = null;
+    }
+
+    @Nullable
+    public Color getColor() {
+        return color;
+    }
 }
--- gui/src/main/java/de/adito/git/gui/FileStatusCellRenderer.java
+++ gui/src/main/java/de/adito/git/gui/FileStatusCellRenderer.java
@@ -18,23 +18,8 @@ public class FileStatusCellRenderer extends DefaultTableCellRenderer {
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         // Text in cells is always displayed as label
         JLabel label = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-        if (table.getModel().getValueAt(row, 2).equals(EChangeType.ADD)) {
-            // Foreground  = text color
-            label.setForeground(new Color(106, 135, 89));
-        } else if (table.getModel().getValueAt(row, 2).equals(EChangeType.NEW)) {
-            label.setForeground(new Color(106, 135, 89));
-        } else if (table.getModel().getValueAt(row, 2).equals(EChangeType.MODIFY)) {
-            label.setForeground(new Color(77, 130, 184));
-        } else if (table.getModel().getValueAt(row, 2).equals(EChangeType.CHANGED)) {
-            label.setForeground(new Color(77, 130, 184));
-        } else if (table.getModel().getValueAt(row, 2).equals(EChangeType.DELETE)) {
-            label.setForeground(new Color(204, 120, 50));
-        } else if (table.getModel().getValueAt(row, 2).equals(EChangeType.MISSING)) {
-            label.setForeground(new Color(204, 120, 50));
-        } else if (table.getModel().getValueAt(row, 2).equals(EChangeType.CONFLICTING)) {
-            label.setForeground(new Color(230, 132, 151));
-        } else {
-            label.setForeground(Color.BLACK);
+        if (!isSelected) {
+            label.setForeground(((EChangeType) table.getModel().getValueAt(row, 2)).getColor());
         }
         return label;
     }
