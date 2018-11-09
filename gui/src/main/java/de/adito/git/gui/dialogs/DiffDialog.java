3a6caece260d638c7c16473c43117dd0866ef6e9
m.kaspera <m.kaspera@adito.de>
2018-11-09 14:17:24 +0100

    DiffDialog now contains a pane with the line numbers
    Pane with the line numbers has the same coloring of lines as the main
    textPane with the contents of the diff

--- gui/src/main/java/de/adito/git/gui/dialogs/DiffDialog.java
+++ gui/src/main/java/de/adito/git/gui/dialogs/DiffDialog.java
@@ -2,6 +2,8 @@ package de.adito.git.gui.dialogs;
 
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
+import de.adito.git.api.data.EChangeSide;
+import de.adito.git.api.data.IFileChangeChunk;
 import de.adito.git.api.data.IFileDiff;
 import de.adito.git.gui.IDiscardable;
 import de.adito.git.gui.TextHighlightUtil;
@@ -10,7 +12,9 @@ import de.adito.git.gui.tableModels.DiffTableModel;
 import io.reactivex.disposables.Disposable;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.StyleConstants;
 import java.awt.*;
 import java.util.List;
 
@@ -23,8 +27,8 @@ class DiffDialog extends JPanel implements IDiscardable {
 
     private final static int SCROLL_SPEED_INCREMENT = 16;
     private final ObservableTable fileListTable = new ObservableTable();
-    private final JTextPane oldVersionPane = new JTextPane();
-    private final JTextPane newVersionPane = new JTextPane();
+    private final DiffPanel oldVersionPanel = new DiffPanel(EChangeSide.OLD);
+    private final DiffPanel newVersionPanel = new DiffPanel(EChangeSide.NEW);
     private Disposable disposable;
     private List<IFileDiff> diffs;
 
@@ -41,9 +45,6 @@ class DiffDialog extends JPanel implements IDiscardable {
         setLayout(new BorderLayout());
         setMinimumSize(new Dimension(800, 600));
         setPreferredSize(new Dimension(1600, 900));
-        // set contentType to text/html. Because for whatever reason that's the only way the whole line gets marked, not just the text
-        oldVersionPane.setContentType("text/html");
-        newVersionPane.setContentType("text/html");
 
         // Table on which to select which IFileDiff is displayed in the DiffPanel
         fileListTable.setModel(new DiffTableModel(diffs));
@@ -58,19 +59,10 @@ class DiffDialog extends JPanel implements IDiscardable {
                 _updateDiffPanel(diffs.get(pSelectedRows[0]));
         });
 
-        // DiffPanel that displays the selected IFileDiff
-        JPanel oldPanel = new JPanel(new BorderLayout());
-        JPanel newPanel = new JPanel(new BorderLayout());
-        oldPanel.add(oldVersionPane, BorderLayout.CENTER);
-        newPanel.add(newVersionPane, BorderLayout.CENTER);
-        JScrollPane oldVersionScrollPane = new JScrollPane(oldPanel, JScrollPane.VERTICAL_SCROLLBAR_NEVER, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        JScrollPane newVersionScrollPane = new JScrollPane(newPanel);
-        newVersionScrollPane.getVerticalScrollBar().setUnitIncrement(SCROLL_SPEED_INCREMENT);
-        // synchronize the two scrollPanes
-        oldVersionScrollPane.getVerticalScrollBar().setModel(newVersionScrollPane.getVerticalScrollBar().getModel());
-        oldVersionScrollPane.setWheelScrollingEnabled(false);
-        oldVersionScrollPane.addMouseWheelListener(newVersionScrollPane::dispatchEvent);
-        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, oldVersionScrollPane, newVersionScrollPane);
+        // make left and right DiffPanel scroll at the same time/speed
+        oldVersionPanel.coupleToScrollPane(newVersionPanel.getMainScrollPane());
+        oldVersionPanel.getMainScrollPane().setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, oldVersionPanel, newVersionPanel);
         splitPane.setResizeWeight(0.5);
 
         // add table and DiffPanel to the Panel
@@ -82,20 +74,164 @@ class DiffDialog extends JPanel implements IDiscardable {
      * @param fileDiff the IFileDiff that should be displayed in the Diff Panel
      */
     private void _updateDiffPanel(IFileDiff fileDiff) {
+        List<IFileChangeChunk> changeChunkList = fileDiff.getFileChanges().getChangeChunks().blockingFirst();
         // clear text in textPanes
-        oldVersionPane.setText("");
-        newVersionPane.setText("");
+        oldVersionPanel.getTextPane().setText("");
+        oldVersionPanel.getLineNumArea().setText("");
+        newVersionPanel.getTextPane().setText("");
+        newVersionPanel.getLineNumArea().setText("");
         // insert the text from the IFileDiffs
         try {
-            TextHighlightUtil.insertChangeChunks(fileDiff.getFileChanges().getChangeChunks().blockingFirst(), oldVersionPane, newVersionPane, true);
+            TextHighlightUtil.insertChangeChunks(changeChunkList, oldVersionPanel.getTextPane(), newVersionPanel.getTextPane(), true);
+            _writeLineNums(changeChunkList);
             revalidate();
         } catch (BadLocationException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * writes the lineNumbers for both panels
+     *
+     * @param pChangeChunkList List of IFileChangeChunks from which the line numbering is extracted
+     */
+    private void _writeLineNums(List<IFileChangeChunk> pChangeChunkList) {
+        Color bgColor = new JLabel().getBackground();
+        int oldVersionLineNum = 1;
+        int newVersionLineNum = 1;
+        int numNewLines;
+        try {
+            for (IFileChangeChunk changeChunk : pChangeChunkList) {
+                numNewLines = changeChunk.getAEnd() - changeChunk.getAStart();
+                TextHighlightUtil.appendText(oldVersionPanel.getLineNumArea().getStyledDocument(), _getLineNumString(oldVersionLineNum, numNewLines), changeChunk.getChangeType());
+                oldVersionLineNum += numNewLines;
+                numNewLines = changeChunk.getBEnd() - changeChunk.getBStart();
+                // numbers on the right DiffPanel should be right-aligned
+                TextHighlightUtil.appendText(
+                        newVersionPanel.getLineNumArea().getStyledDocument(),
+                        _getLineNumString(newVersionLineNum, numNewLines),
+                        changeChunk.getChangeType().getDiffColor(),
+                        StyleConstants.ALIGN_RIGHT);
+                newVersionLineNum += numNewLines;
+                // parity lines should only contain newlines anyway, so no filtering or counting newlines should be necessary
+                TextHighlightUtil.appendText(oldVersionPanel.getLineNumArea().getStyledDocument(), changeChunk.getAParityLines(), bgColor);
+                TextHighlightUtil.appendText(newVersionPanel.getLineNumArea().getStyledDocument(), changeChunk.getBParityLines(), bgColor);
+            }
+        } catch (BadLocationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param pStart number of the first line
+     * @param pCount number of lines
+     * @return String with the lineNumbers from pStart to pStart + pCount
+     */
+    private String _getLineNumString(int pStart, int pCount) {
+        StringBuilder lineNumStringBuilder = new StringBuilder();
+        for (int index = 0; index < pCount; index++) {
+            lineNumStringBuilder.append(pStart + index).append("\n");
+        }
+        return lineNumStringBuilder.toString();
+    }
+
     @Override
     public void discard() {
         disposable.dispose();
+    }
+
+    /**
+     * Class to handle the basic layout of the two panels that display the differences between two files
+     */
+    private class DiffPanel extends JPanel {
+
+        private final EChangeSide changeSide;
+        private final JTextPane lineNumArea = new JTextPane();
+        private final JTextPane textPane = new JTextPane();
+        private final JScrollPane mainScrollPane = new JScrollPane();
+        private final JScrollPane lineScrollPane = new JScrollPane();
+
+        DiffPanel(EChangeSide pChangeSide) {
+            changeSide = pChangeSide;
+            _initGui();
+        }
+
+        private void _initGui() {
+            setLayout(new BorderLayout());
+
+            // set contentType to text/html. Because for whatever reason that's the only way the whole line gets marked, not just the text
+            textPane.setContentType("text/html");
+            lineNumArea.setContentType("text/html");
+
+            // textPane should no be editable, but the text should still be normal
+            textPane.setEditable(false);
+
+            // text here should look disabled/grey and not be editable
+            lineNumArea.setEnabled(false);
+
+            // ScrollPane setup
+            mainScrollPane.add(textPane);
+            mainScrollPane.setViewportView(textPane);
+            mainScrollPane.getVerticalScrollBar().setUnitIncrement(SCROLL_SPEED_INCREMENT);
+            lineScrollPane.add(lineNumArea);
+            lineScrollPane.setViewportView(lineNumArea);
+            lineScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+            lineScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+            _coupleScrollPanes(mainScrollPane, lineScrollPane);
+
+            // add the parts to the DiffPanel
+            add(lineScrollPane, changeSide == EChangeSide.OLD ? BorderLayout.EAST : BorderLayout.WEST);
+            add(mainScrollPane, BorderLayout.CENTER);
+
+            // remove Borders from the parts and use the set Border to draw a border around the whole panel
+            Border usedBorder = lineScrollPane.getBorder();
+            lineScrollPane.setBorder(null);
+            mainScrollPane.setBorder(null);
+            setBorder(usedBorder);
+        }
+
+        /**
+         * makes this DiffPanel's scrolling fixed to the scrolling of the passed JScrollPane
+         *
+         * @param pScrollPane JScrollPane that should determine the scrolling of the textPane and the lineNumbers of this DiffPanel
+         */
+        void coupleToScrollPane(JScrollPane pScrollPane) {
+            _coupleScrollPanes(pScrollPane, mainScrollPane);
+            _coupleScrollPanes(pScrollPane, lineScrollPane);
+        }
+
+        /**
+         * @return the ScrollPane that controls the Scrolling of the main textPane
+         */
+        JScrollPane getMainScrollPane() {
+            return mainScrollPane;
+        }
+
+        /**
+         * @return the JTextPane that contains the diff-text
+         */
+        JTextPane getTextPane() {
+            return textPane;
+        }
+
+        /**
+         * @return JTextPane that contains only the line-numbering
+         */
+        JTextPane getLineNumArea() {
+            return lineNumArea;
+        }
+
+        /**
+         * Makes the masterScrollPane determine the scrolling behaviour/speed of the slaveScrollPane.
+         * Both Panes are then  intertwined and cannot be scrolled independently
+         *
+         * @param masterScrollPane ScrollPane that will control scrolling behaviour. Is notified when the slave scrolls and does scroll then, too
+         * @param slaveScrollPane  ScrollPane that is linked to the master. Scrolling in the master also means scrolling in the slave, and vice versa
+         */
+        private void _coupleScrollPanes(JScrollPane masterScrollPane, JScrollPane slaveScrollPane) {
+            slaveScrollPane.getVerticalScrollBar().setModel(masterScrollPane.getVerticalScrollBar().getModel());
+            slaveScrollPane.setWheelScrollingEnabled(false);
+            slaveScrollPane.addMouseWheelListener(masterScrollPane::dispatchEvent);
+        }
     }
 }
