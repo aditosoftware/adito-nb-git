c6aa1651b15fd7f1646c13cbe7b16da86e2c9805
m.kaspera <m.kaspera@adito.de>
2018-11-09 13:55:33 +0100

    Additional methods for different cases (includes alignment, text color)

--- gui/src/main/java/de/adito/git/gui/TextHighlightUtil.java
+++ gui/src/main/java/de/adito/git/gui/TextHighlightUtil.java
@@ -31,11 +31,11 @@ public class TextHighlightUtil {
         StyledDocument bDocument = bTextPane.getStyledDocument();
         for (IFileChangeChunk changeChunk : changeChunks) {
             // Background color according to the EChangeType
-            TextHighlightUtil.insertText(aDocument, changeChunk.getALines(), changeChunk.getChangeType().getDiffColor());
-            TextHighlightUtil.insertText(bDocument, changeChunk.getBLines(), changeChunk.getChangeType().getDiffColor());
+            TextHighlightUtil.appendText(aDocument, changeChunk.getALines(), changeChunk.getChangeType().getDiffColor());
+            TextHighlightUtil.appendText(bDocument, changeChunk.getBLines(), changeChunk.getChangeType().getDiffColor());
             if (insertParityStrings) {
-                TextHighlightUtil.insertText(aDocument, changeChunk.getAParityLines(), changeChunk.getChangeType().getDiffColor());
-                TextHighlightUtil.insertText(bDocument, changeChunk.getBParityLines(), changeChunk.getChangeType().getDiffColor());
+                TextHighlightUtil.appendText(aDocument, changeChunk.getAParityLines(), changeChunk.getChangeType().getDiffColor());
+                TextHighlightUtil.appendText(bDocument, changeChunk.getBParityLines(), changeChunk.getChangeType().getDiffColor());
             }
         }
     }
@@ -67,21 +67,59 @@ public class TextHighlightUtil {
      * @param changeType ChangeType, determines the background color of the line/text
      * @throws BadLocationException if the message is getting inserted out of bounds
      */
-    public static void insertText(StyledDocument document, String message, EChangeType changeType) throws BadLocationException {
-        insertText(document, message, changeType.getDiffColor());
+    public static void appendText(StyledDocument document, String message, EChangeType changeType) throws BadLocationException {
+        appendText(document, message, changeType.getDiffColor());
     }
 
     /**
      * @param document StyledDocument of the JTextPane for which to insert the message
      * @param message  String that should be inserted into the JTextPane
-     * @param color    The Background color for the Text getting inserted
+     * @param bgColor  The Background color for the Text getting inserted
      * @throws BadLocationException if the message is getting inserted out of bounds
      */
-    public static void insertText(@NotNull StyledDocument document, @NotNull String message, @Nullable Color color) throws BadLocationException {
-        if (color == null)
-            color = new JLabel().getBackground();
+    public static void appendText(@NotNull StyledDocument document, @NotNull String message, @Nullable Color bgColor) throws BadLocationException {
+        appendText(document, message, bgColor, new JLabel().getForeground());
+    }
+
+    /**
+     * @param document  StyledDocument of the JTextPane for which to insert the message
+     * @param message   String that should be inserted into the JTextPane
+     * @param bgColor   The Background color for the Text getting inserted
+     * @param alignment which alignment the text should have, default is left-aligned (0)
+     * @throws BadLocationException if the message is getting inserted out of bounds
+     */
+    public static void appendText(@NotNull StyledDocument document, @NotNull String message, @Nullable Color bgColor, int alignment) throws BadLocationException {
+        appendText(document, message, bgColor, new JLabel().getForeground(), alignment);
+    }
+
+    /**
+     * @param document  StyledDocument of the JTextPane for which to insert the message
+     * @param message   String that should be inserted into the JTextPane
+     * @param bgColor   The Background color for the Text getting inserted
+     * @param textColor The color that the text is drawn in
+     * @throws BadLocationException if the message is getting inserted out of bounds
+     */
+    private static void appendText(@NotNull StyledDocument document, @NotNull String message, @Nullable Color bgColor, @Nullable Color textColor) throws BadLocationException {
+        appendText(document, message, bgColor, textColor, StyleConstants.ALIGN_LEFT);
+    }
+
+    /**
+     * @param document  StyledDocument of the JTextPane for which to insert the message
+     * @param message   String that should be inserted into the JTextPane
+     * @param bgColor   The Background color for the Text getting inserted
+     * @param textColor The color that the text is drawn in
+     * @param alignment which alignment the text should have, default is left-aligned (0)
+     * @throws BadLocationException if the message is getting inserted out of bounds
+     */
+    private static void appendText(@NotNull StyledDocument document, @NotNull String message, @Nullable Color bgColor, @Nullable Color textColor, int alignment) throws BadLocationException {
+        if (bgColor == null)
+            bgColor = new JLabel().getBackground();
+        if (textColor == null)
+            textColor = new JLabel().getForeground();
         SimpleAttributeSet attr = new SimpleAttributeSet();
-        StyleConstants.setBackground(attr, color);
+        StyleConstants.setBackground(attr, bgColor);
+        StyleConstants.setForeground(attr, textColor);
+        StyleConstants.setAlignment(attr, alignment);
         int offset = document.getLength();
         document.insertString(offset, message, attr);
     }
