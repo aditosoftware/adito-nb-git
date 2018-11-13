c4ff2208a2cd002221f2db0533511e138f19ec89
m.kaspera <m.kaspera@adito.de>
2018-11-13 14:55:20 +0100

    Storage of pre-determined, application-wide constants

---
+++ gui/src/main/java/de/adito/git/gui/Constants.java
@@ -0,0 +1,16 @@
+package de.adito.git.gui;
+
+/**
+ * Central space to store variable definitions that are used in several places
+ * and who are valid and pre-determined in the scope of the whole application
+ *
+ * @author m.kaspera 12.11.2018
+ */
+public class Constants {
+
+    public static final int SCROLL_SPEED_INCREMENT = 16;
+    public static final String ACCEPT_CHANGE_YOURS_ICON = "/de/adito/git/gui/icons/acceptChange.png";
+    public static final String ACCEPT_CHANGE_THEIRS_ICON = "/de/adito/git/gui/icons/acceptChangeRight.png";
+    public static final String DISCARD_CHANGE_ICON = "/de/adito/git/gui/icons/discardChange.png";
+
+}
