68bdd812b313a2aa0cbeb8791719b04cbdd232a2
m.kaspera <m.kaspera@adito.de>
2018-11-29 10:18:10 +0100

    Made column names public + can find column by name

--- gui/src/main/java/de/adito/git/gui/tableModels/CommitHistoryTreeListTableModel.java
+++ gui/src/main/java/de/adito/git/gui/tableModels/CommitHistoryTreeListTableModel.java
@@ -1,6 +1,7 @@
 package de.adito.git.gui.tableModels;
 
 import de.adito.git.api.CommitHistoryTreeListItem;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.table.AbstractTableModel;
 import java.util.ArrayList;
@@ -15,12 +16,24 @@ import java.util.List;
 
 public class CommitHistoryTreeListTableModel extends AbstractTableModel {
 
+    public static final String BRANCHING_COL_NAME = "Branching/Commit Message";
+    public static final String AUTHOR_COL_NAME = "Author";
+    public static final String DATE_COL_NAME = "Date";
+
     private List<CommitHistoryTreeListItem> commitList;
     private static final int BRANCHING = 0;
     private static final int AUTHOR = 1;
     private static final int TIME = 2;
 
-    private static final List<String> columnNames = new ArrayList<>(Arrays.asList("Branches", "Author", "Time"));
+    private static final List<String> columnNames = new ArrayList<>(Arrays.asList(BRANCHING_COL_NAME, AUTHOR_COL_NAME, DATE_COL_NAME));
+
+    public static int getColumnIndex(@NotNull String columnName) {
+        for (int index = 0; index < columnNames.size(); index++) {
+            if (columnName.equals(columnNames.get(index)))
+                return index;
+        }
+        return -1;
+    }
 
     /**
      * @param pCommitList the list of commits to show
