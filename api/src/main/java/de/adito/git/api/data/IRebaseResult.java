26491c546338c37c04a9f765afa0f2132edf14ea
m.kaspera <m.kaspera@adito.de>
2018-12-05 15:42:31 +0100

    Result classes for Rebasing

---
+++ api/src/main/java/de/adito/git/api/data/IRebaseResult.java
@@ -0,0 +1,30 @@
+package de.adito.git.api.data;
+
+import java.util.List;
+
+/**
+ * Interface that defines the class that keeps the result of a Rebase operation
+ *
+ * @author m.kaspera 05.12.2018
+ */
+public interface IRebaseResult
+{
+
+  /**
+   * FINISHED:              Rebase finished successfully
+   * ABORTED:               Rebase operation was aborted
+   * UP_TO_DATE:            Rebase didn't do anything, files were already up to date
+   * STOPPED:               Rebase stopped, probably due to a conflict
+   * FAILED:                Rebase operation failed
+   * EXISTING_CONFLICTS:    Rebase operation cannot continue because there are still existing conflicts
+   */
+  enum ResultType
+  {FINISHED, ABORTED, UP_TO_DATE, STOPPED, FAILED, EXISTING_CONFLICTS}
+
+  List<IMergeDiff> getMergeConflicts();
+
+  ResultType getResultType();
+
+  boolean isSuccess();
+
+}
---
+++ impl/src/main/java/de/adito/git/impl/data/RebaseResultImpl.java
@@ -0,0 +1,70 @@
+package de.adito.git.impl.data;
+
+import de.adito.git.api.data.*;
+import org.eclipse.jgit.api.RebaseResult;
+
+import java.util.List;
+
+/**
+ * Class that keeps the result of a Rebase operation
+ *
+ * @author Michael Kaspera 05.12.2018
+ */
+public class RebaseResultImpl implements IRebaseResult
+{
+
+  private List<IMergeDiff> conflicts;
+  private RebaseResult.Status status;
+
+  /**
+   * @param pConflicts List with IMergeDiffs describing the conflicts
+   * @param pStatus    the status of the undertaken Rebase operation
+   */
+  public RebaseResultImpl(List<IMergeDiff> pConflicts, RebaseResult.Status pStatus)
+  {
+    conflicts = pConflicts;
+    status = pStatus;
+  }
+
+  @Override
+  public List<IMergeDiff> getMergeConflicts()
+  {
+    return conflicts;
+  }
+
+  @Override
+  public ResultType getResultType()
+  {
+    ResultType returnValue;
+    switch (status)
+    {
+      case ABORTED:
+        returnValue = ResultType.ABORTED;
+        break;
+      case STOPPED:
+        returnValue = ResultType.STOPPED;
+        break;
+      case OK:
+        returnValue = ResultType.FINISHED;
+        break;
+      case UP_TO_DATE:
+        returnValue = ResultType.UP_TO_DATE;
+        break;
+      case FAILED:
+        returnValue = ResultType.FAILED;
+        break;
+      case CONFLICTS:
+        returnValue = ResultType.EXISTING_CONFLICTS;
+        break;
+      default:
+        returnValue = null;
+    }
+    return returnValue;
+  }
+
+  @Override
+  public boolean isSuccess()
+  {
+    return status.isSuccessful();
+  }
+}
