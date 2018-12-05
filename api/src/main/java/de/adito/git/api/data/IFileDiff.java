7519eece8a105a4f3ee8c373f02030a7768363ec
m.kaspera <m.kaspera@adito.de>
2018-12-05 07:47:38 +0100

    Changes to accommodate SonarQube

--- api/src/main/java/de/adito/git/api/data/IFileDiff.java
+++ api/src/main/java/de/adito/git/api/data/IFileDiff.java
@@ -1,41 +1,45 @@
 package de.adito.git.api.data;
 
 /**
+ * Interface for the data object containing the information about the changes to a file
+ *
  * @author m.kaspera 20.09.2018
  */
-public interface IFileDiff {
+public interface IFileDiff
+{
 
-    /**
-     *
-     * @param side {@link EChangeSide} that tells if the older or later branch/commit/... should be inspected
-     * @return the identifier for the file/object on the specified side of the tree
-     */
-    String getId(EChangeSide side);
+  /**
+   * returns the Id for the given side of this IFileDiff
+   *
+   * @param pSide {@link EChangeSide} that tells if the older or later branch/commit/... should be inspected
+   * @return the identifier for the file/object on the specified side of the tree
+   */
+  String getId(EChangeSide pSide);
 
-    /**
-     *
-     * @return {@link EChangeType} that tells which kind of change happened (add/remove...)
-     */
-    EChangeType getChangeType();
+  /**
+   * @return {@link EChangeType} that tells which kind of change happened (add/remove...)
+   */
+  EChangeType getChangeType();
 
-    /**
-     *
-     * @param side {@link EChangeSide} that tells if the older or later branch/commit/... should be inspected
-     * @return {@link EFileType} which kind of file
-     */
-    EFileType getFileType(EChangeSide side);
+  /**
+   * returns the type of file for this IFileDiff
+   *
+   * @param pSide {@link EChangeSide} that tells if the older or later branch/commit/... should be inspected
+   * @return {@link EFileType} which kind of file
+   */
+  EFileType getFileType(EChangeSide pSide);
 
-    /**
-     *
-     * @param side {@link EChangeSide} that tells if the older or later branch/commit/... should be inspected
-     * @return the path from root to the file
-     */
-    String getFilePath(EChangeSide side);
+  /**
+   * returns filePath for the given side
+   *
+   * @param pChangeSide {@link EChangeSide} that tells if the older or later branch/commit/... should be inspected
+   * @return the path from root to the file
+   */
+  String getFilePath(EChangeSide pChangeSide);
 
-    /**
-     *
-     * @return {@link IFileChanges} that contains a list detailing the changes in each changed line
-     */
-    IFileChanges getFileChanges();
+  /**
+   * @return {@link IFileChanges} that contains a list detailing the changes in each changed line
+   */
+  IFileChanges getFileChanges();
 
 }
--- impl/src/main/java/de/adito/git/impl/data/FileDiffImpl.java
+++ impl/src/main/java/de/adito/git/impl/data/FileDiffImpl.java
@@ -1,12 +1,8 @@
 package de.adito.git.impl.data;
 
-import de.adito.git.api.data.EChangeSide;
-import de.adito.git.api.data.EChangeType;
-import de.adito.git.api.data.EFileType;
-import de.adito.git.api.data.IFileDiff;
+import de.adito.git.api.data.*;
 import de.adito.git.impl.EnumMappings;
-import org.eclipse.jgit.diff.DiffEntry;
-import org.eclipse.jgit.diff.EditList;
+import org.eclipse.jgit.diff.*;
 import org.eclipse.jgit.patch.FileHeader;
 
 /**
@@ -14,62 +10,70 @@ import org.eclipse.jgit.patch.FileHeader;
  *
  * @author m.kaspera 21.09.2018
  */
-public class FileDiffImpl implements IFileDiff {
+public class FileDiffImpl implements IFileDiff
+{
 
-    private DiffEntry diffEntry;
-    private FileHeader fileHeader;
-    private FileChangesImpl fileChanges;
-    private String originalFileContents;
-    private String newFileContents;
+  private DiffEntry diffEntry;
+  private FileHeader fileHeader;
+  private FileChangesImpl fileChanges;
+  private String originalFileContents;
+  private String newFileContents;
 
-    public FileDiffImpl(DiffEntry pDiffEntry, FileHeader pFileHeader, String pOriginalFileContents, String pNewFileContents) {
-        diffEntry = pDiffEntry;
-        fileHeader = pFileHeader;
-        originalFileContents = pOriginalFileContents;
-        newFileContents = pNewFileContents;
+  public FileDiffImpl(DiffEntry pDiffEntry, FileHeader pFileHeader, String pOriginalFileContents, String pNewFileContents)
+  {
+    diffEntry = pDiffEntry;
+    fileHeader = pFileHeader;
+    originalFileContents = pOriginalFileContents;
+    newFileContents = pNewFileContents;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String getId(EChangeSide pSide)
+  {
+    return (pSide == EChangeSide.NEW ? diffEntry.getNewId() : diffEntry.getOldId()).toString();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public EChangeType getChangeType()
+  {
+    return EnumMappings._toEChangeType(diffEntry.getChangeType());
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public EFileType getFileType(EChangeSide pSide)
+  {
+    return EnumMappings._toEFileType(pSide == EChangeSide.NEW ? diffEntry.getMode(DiffEntry.Side.NEW) : diffEntry.getMode(DiffEntry.Side.OLD));
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String getFilePath(EChangeSide pSide)
+  {
+    return pSide == EChangeSide.NEW ? diffEntry.getNewPath() : diffEntry.getOldPath();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public FileChangesImpl getFileChanges()
+  {
+    if (fileChanges == null)
+    {
+      EditList edits = fileHeader.getHunks().get(0).toEditList();
+      fileChanges = new FileChangesImpl(edits, originalFileContents, newFileContents);
     }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public String getId(EChangeSide side) {
-        return (side == EChangeSide.NEW ? diffEntry.getNewId() : diffEntry.getOldId()).toString();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public EChangeType getChangeType() {
-        return EnumMappings._toEChangeType(diffEntry.getChangeType());
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public EFileType getFileType(EChangeSide side) {
-        return EnumMappings._toEFileType(side == EChangeSide.NEW ? diffEntry.getMode(DiffEntry.Side.NEW) : diffEntry.getMode(DiffEntry.Side.OLD));
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public String getFilePath(EChangeSide side) {
-        return side == EChangeSide.NEW ? diffEntry.getNewPath() : diffEntry.getOldPath();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public FileChangesImpl getFileChanges() {
-        if (fileChanges == null) {
-            EditList edits = fileHeader.getHunks().get(0).toEditList();
-            fileChanges = new FileChangesImpl(edits, originalFileContents, newFileContents);
-        }
-        return fileChanges;
-    }
+    return fileChanges;
+  }
 }
