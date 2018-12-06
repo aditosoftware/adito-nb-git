7223b0f853805cccbcd05113ccf19e94c9904f5b
m.kaspera <m.kaspera@adito.de>
2018-12-06 10:10:33 +0100

    Recursive version of doRebase -> Iterative version

--- gui/src/main/java/de/adito/git/gui/actions/PullAction.java
+++ gui/src/main/java/de/adito/git/gui/actions/PullAction.java
@@ -2,14 +2,14 @@ package de.adito.git.gui.actions;
 
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
-import de.adito.git.api.IRepository;
-import de.adito.git.api.data.IRebaseResult;
+import de.adito.git.api.*;
+import de.adito.git.api.data.*;
 import de.adito.git.gui.dialogs.*;
 import io.reactivex.Observable;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
-import java.util.Optional;
+import java.util.*;
 
 /**
  * pullAction to pull from one branch.
@@ -41,40 +41,57 @@ class PullAction extends AbstractAction
   @Override
   public void actionPerformed(ActionEvent pEvent)
   {
-    Runnable attemptMergeRunnable = () -> _doRebase(false);
+    Runnable attemptMergeRunnable = this::_doRebase;
     new Thread(attemptMergeRunnable).start();
   }
 
   /**
-   * Recursive method that calls the pull method as long as there are merge conflicts/
-   * the user didn't press abort or cancel/pull doesn't return a success message
-   *
-   * @param pDoAbort if the rebase should be aborted
+   * Keeps calling the pull method of the repository until the result is either a success or the user
+   * presses cancel on one of the Conflict resolution dialogs
    */
-  private void _doRebase(boolean pDoAbort)
+  private void _doRebase()
   {
+    boolean doAbort = false;
     try
     {
-      IRebaseResult rebaseResult =
-          repository.blockingFirst().orElseThrow(() -> new RuntimeException("no valid repository found")).pull(pDoAbort);
-      // if the user aborted the rebase or the rebase finished successfully -> break recursion
-      if (pDoAbort && rebaseResult.getResultType() == IRebaseResult.ResultType.ABORTED || rebaseResult.isSuccess())
-        return;
-      if (!rebaseResult.getMergeConflicts().isEmpty())
+      while (!doAbort)
       {
-        DialogResult dialogResult = dialogProvider.showMergeConflictDialog(repository, rebaseResult.getMergeConflicts());
-        if (!dialogResult.isPressedOk())
+        IRebaseResult rebaseResult =
+            repository.blockingFirst().orElseThrow(() -> new RuntimeException("no valid repository found")).pull(false);
+        if (rebaseResult.isSuccess())
+          break;
+        if (!rebaseResult.getMergeConflicts().isEmpty())
         {
-          // user pressed cancel -> abort
-          _doRebase(true);
+          // if the pull should be aborted, _handleConflictDialog returns true
+          doAbort = _handleConflictDialog(rebaseResult.getMergeConflicts());
         }
       }
-      // if the user pressed OK try and resume the process
-      _doRebase(false);
     }
     catch (Exception e)
     {
       throw new RuntimeException(e);
     }
+  }
+
+  /**
+   * Handles showing the conflict dialog if a conflict occurred during the pull
+   *
+   * @param pMergeConflicts List of IMergeDiff for each file that is in a conflicting state
+   * @return true if the user pressed cancel and the pull should be aborted, false otherwise
+   * @throws AditoGitException if an error occurred during the pull
+   */
+  private boolean _handleConflictDialog(List<IMergeDiff> pMergeConflicts) throws AditoGitException
+  {
+    DialogResult dialogResult = dialogProvider.showMergeConflictDialog(repository, pMergeConflicts);
+    if (!dialogResult.isPressedOk())
+    {
+      IRebaseResult abortedRebaseResult =
+          repository.blockingFirst().orElseThrow(() -> new RuntimeException("no valid repository found")).pull(true);
+      if (abortedRebaseResult.getResultType() != IRebaseResult.ResultType.ABORTED)
+        throw new RuntimeException("The abort of the rebase failed with state: " + abortedRebaseResult.getResultType());
+      // user pressed cancel -> abort
+      return true;
+    }
+    return false;
   }
 }
