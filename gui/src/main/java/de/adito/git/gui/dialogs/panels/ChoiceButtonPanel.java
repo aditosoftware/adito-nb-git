bec1bc2f65f78ec84de8bd5d0ab6c1e34886abe2
m.kaspera <m.kaspera@adito.de>
2018-12-17 16:56:52 +0100

    Passing null as discardIcon, fixes stuck icons

--- gui/src/main/java/de/adito/git/gui/dialogs/panels/ChoiceButtonPanel.java
+++ gui/src/main/java/de/adito/git/gui/dialogs/panels/ChoiceButtonPanel.java
@@ -1,22 +1,26 @@
 package de.adito.git.gui.dialogs.panels;
 
-import de.adito.git.api.data.*;
+import de.adito.git.api.data.EChangeType;
+import de.adito.git.api.data.IFileChangeChunk;
 import de.adito.git.gui.IDiscardable;
 import io.reactivex.Observable;
 import io.reactivex.disposables.Disposable;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.event.ActionListener;
+import java.util.Collections;
 import java.util.List;
-import java.util.*;
 import java.util.function.Consumer;
 
 public class ChoiceButtonPanel implements IDiscardable
 {
 
   private static final int ADDITIONAL_HEIGHT_PIXELS = 3;
-  private static final int BUTTON_BAR_WIDTH = 35;
   private static final int HALF_BUTTON_HEIGHT = 8;
+  private int buttonBarWidth = 35;
 
   private final JPanel buttonBar = new JPanel();
   private final JScrollPane buttonPanelScrollPane = new JScrollPane(buttonBar, ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER,
@@ -38,6 +42,9 @@ public class ChoiceButtonPanel implements IDiscardable
     lineHeight = pLineHeight;
     doOnDiscard = pDoOnDiscard;
     doOnAccept = pDoOnAccept;
+    if (pDiscardIcon == null)
+      buttonBarWidth = 18;
+    buttonPanelScrollPane.setBorder(null);
     disposable = pModel.getFileDiff().switchMap(pFileDiff -> pFileDiff
         .map(pDiff -> pDiff.getFileChanges().getChangeChunks())
         .orElse(Observable.just(Collections.emptyList())))
@@ -52,9 +59,10 @@ public class ChoiceButtonPanel implements IDiscardable
 
   private void _initButtonPanel(List<IFileChangeChunk> pChangeChunks)
   {
+    _clearPanel(buttonBar);
     buttonBar.setLayout(new BoxLayout(buttonBar, BoxLayout.Y_AXIS));
-    buttonBar.setMinimumSize(new Dimension(BUTTON_BAR_WIDTH, 0));
-    buttonBar.setMaximumSize(new Dimension(BUTTON_BAR_WIDTH, Integer.MAX_VALUE));
+    buttonBar.setMinimumSize(new Dimension(buttonBarWidth, 0));
+    buttonBar.setMaximumSize(new Dimension(buttonBarWidth, Integer.MAX_VALUE));
     // since the mergePanel has no parityLines, the number of Lines * the height of one line should be the total size of the other components
     // start with -3 because else the the buttons are not aligned with the lines, probably due to borders or something
     int heightBefore = -ADDITIONAL_HEIGHT_PIXELS;
@@ -65,7 +73,13 @@ public class ChoiceButtonPanel implements IDiscardable
         heightBefore = _insertButtonsForChunk(changeChunk, heightBefore, lineHeight);
       }
     }
+    if (!pChangeChunks.isEmpty())
+    {
+      buttonBar.add(Box.createRigidArea(
+          new Dimension(0, ((pChangeChunks.get(pChangeChunks.size() - 1).getBEnd() + 1) * lineHeight - (heightBefore)) + ADDITIONAL_HEIGHT_PIXELS)));
+    }
     buttonBar.add(Box.createGlue());
+    buttonBar.revalidate();
   }
 
   /**
@@ -76,17 +90,20 @@ public class ChoiceButtonPanel implements IDiscardable
   private int _insertButtonsForChunk(IFileChangeChunk pChangeChunk, int pHeightBefore, int pLineHeight)
   {
     JPanel dualButtonPanel = new JPanel(new BorderLayout());
-    dualButtonPanel.setMaximumSize(new Dimension(BUTTON_BAR_WIDTH, discardIcon.getIconHeight()));
-    JButton discardChangeButton = _createButton(discardIcon, "discard change");
+    dualButtonPanel.setMaximumSize(new Dimension(buttonBarWidth, acceptIcon.getIconHeight()));
+    if (discardIcon != null)
+    {
+      JButton discardChangeButton = _createButton(discardIcon, "discard change");
+      discardChangeButton.setBorder(null);
+      dualButtonPanel.add(discardChangeButton, BorderLayout.CENTER);
+      discardChangeButton.addActionListener(e -> doOnDiscard.accept(pChangeChunk));
+    }
     JButton acceptChangeButton = _createButton(acceptIcon, "accept this change");
     acceptChangeButton.setBorder(null);
-    discardChangeButton.setBorder(null);
-    dualButtonPanel.add(discardChangeButton, BorderLayout.CENTER);
     dualButtonPanel.add(acceptChangeButton, orientation);
     int currentHeight = (int) ((pChangeChunk.getBStart() + (double) (pChangeChunk.getBEnd() - pChangeChunk.getBStart()) / 2) * pLineHeight);
     buttonBar.add(Box.createRigidArea(new Dimension(0, (currentHeight - (pHeightBefore + HALF_BUTTON_HEIGHT)))));
     buttonBar.add(dualButtonPanel);
-    discardChangeButton.addActionListener(e -> doOnDiscard.accept(pChangeChunk));
     acceptChangeButton.addActionListener(e -> doOnAccept.accept(pChangeChunk));
     return currentHeight + HALF_BUTTON_HEIGHT;
   }
@@ -96,6 +113,28 @@ public class ChoiceButtonPanel implements IDiscardable
     JButton createdButton = new JButton(pIcon);
     createdButton.setToolTipText(pToolTip);
     return createdButton;
+  }
+
+  /**
+   * Removes all Components registered on the JPanel and re-validates/repaints it
+   *
+   * @param pPanel JPanel from which to remove all Components
+   */
+  private void _clearPanel(JPanel pPanel)
+  {
+    for (Component component : pPanel.getComponents())
+    {
+      if (component instanceof JButton)
+      {
+        for (ActionListener actionListener : ((JButton) component).getActionListeners())
+        {
+          ((JButton) component).removeActionListener(actionListener);
+        }
+      }
+    }
+    pPanel.removeAll();
+    pPanel.validate();
+    pPanel.repaint();
   }
 
   @Override
