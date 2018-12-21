cf8533d2e5c39b8d74f17b1234a372b93b95e28a
m.kaspera <m.kaspera@adito.de>
2018-12-21 16:21:38 +0100

    Class that holds the information about the user credentials

---
+++ impl/src/main/java/de/adito/git/impl/ssh/GitUserInfo.java
@@ -0,0 +1,116 @@
+package de.adito.git.impl.ssh;
+
+import com.google.inject.Inject;
+import com.google.inject.assistedinject.Assisted;
+import com.jcraft.jsch.UserInfo;
+import de.adito.git.api.IKeyStore;
+import de.adito.git.api.IUserInputPrompt;
+
+import javax.annotation.Nullable;
+import java.io.File;
+import java.util.Arrays;
+
+/**
+ * Holds the information about the password/passphrase/ssh key location for the user
+ * Also has the methods that get called by the Jsch session to prompt the user for input if any of these information are missing
+ *
+ * @author m.kaspera, 20.12.2018
+ */
+public class GitUserInfo implements UserInfo
+{
+
+  private final IUserInputPrompt userInputPrompt;
+  private final IKeyStore keyStore;
+  private String passPhrase;
+  private String password;
+  private File sshKeyFile;
+
+  @Inject
+  GitUserInfo(IUserInputPrompt pUserInputPrompt, IKeyStore pKeyStore,
+              @Nullable @Assisted("passphrase") String pPassPhrase,
+              @Nullable @Assisted("password") String pPassword)
+  {
+    keyStore = pKeyStore;
+    passPhrase = pPassPhrase;
+    password = pPassword;
+    userInputPrompt = pUserInputPrompt;
+    sshKeyFile = null;
+  }
+
+  void setPassPhrase(String pPassPhrase)
+  {
+    passPhrase = pPassPhrase;
+  }
+
+  void setPassword(String pPassword)
+  {
+    password = pPassword;
+  }
+
+  @Override
+  public String getPassphrase()
+  {
+    return passPhrase;
+  }
+
+  @Override
+  public String getPassword()
+  {
+    return password;
+  }
+
+  public File getSshKeyFile()
+  {
+    return sshKeyFile;
+  }
+
+  public void setSshKeyFile(File pSshKeyFile)
+  {
+    sshKeyFile = pSshKeyFile;
+  }
+
+  @Override
+  public boolean promptPassword(String pMessage)
+  {
+    IUserInputPrompt.PromptResult result = userInputPrompt.promptPassword(pMessage);
+    password = result.getUserInput();
+    return result.isPressedOK();
+  }
+
+  @Override
+  public boolean promptPassphrase(String pMessage)
+  {
+    if (sshKeyFile != null)
+    {
+      char[] savedPassphrase = keyStore.read(sshKeyFile.getAbsolutePath());
+      if (savedPassphrase != null)
+      {
+        passPhrase = String.valueOf(savedPassphrase);
+        // null out the char array with the password
+        Arrays.fill(savedPassphrase, '0');
+        return true;
+      }
+
+    }
+    IUserInputPrompt.PromptResult result = userInputPrompt.promptPassphrase(pMessage);
+    passPhrase = result.getUserInput();
+    if (sshKeyFile != null && passPhrase != null && !passPhrase.isEmpty())
+    {
+      keyStore.save(sshKeyFile.getAbsolutePath(), result.getUserInput().toCharArray(), null);
+    }
+    return result.isPressedOK();
+  }
+
+  @Override
+  public boolean promptYesNo(String pMessage)
+  {
+    return userInputPrompt.promptYesNo(pMessage).isPressedOK();
+  }
+
+  @Override
+  public void showMessage(String pMessage)
+  {
+    throw new UnsupportedOperationException();
+  }
+
+}
