2f1110215070e9c80d01eefb9f7c8522921ec67f
A.Arnold <a.arnold@adito.de>
2019-01-16 09:56:07 +0100

    added a browse button to the GitconfigDialog

--- gui/src/main/java/de/adito/git/gui/dialogs/GitConfigDialog.java
+++ gui/src/main/java/de/adito/git/gui/dialogs/GitConfigDialog.java
@@ -3,16 +3,13 @@ package de.adito.git.gui.dialogs;
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
 import de.adito.git.api.IRepository;
-import de.adito.git.gui.Constants;
+import de.adito.git.gui.*;
+import info.clearthought.layout.TableLayout;
 import io.reactivex.Observable;
 
 import javax.swing.*;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import javax.swing.filechooser.FileSystemView;
+import java.util.*;
 
 /**
  * @author m.kaspera, 24.12.2018
@@ -38,19 +35,37 @@ public class GitConfigDialog extends AditoBaseDialog<Map<String, String>>
 
   private void _initGui()
   {
-    setLayout(new GridBagLayout());
-    GridBagConstraints gbc = new GridBagConstraints();
-    gbc.insets = new Insets(5, 10, 5, 10);
-    gbc.fill = GridBagConstraints.HORIZONTAL;
-    gbc.gridx = 0;
-    add(new JLabel(SSH_KEY_FIELD_LABEL), gbc);
-    gbc.gridx = 1;
-    add(sshKeyField, gbc);
-    gbc.gridx = 0;
-    gbc.gridy = 1;
-    add(new JLabel(SSH_PASSPHRASE_FIELD_LABEL), gbc);
-    gbc.gridx = 1;
-    add(sshPassphraseField, gbc);
+    double fill = TableLayout.FILL;
+    double pref = TableLayout.PREFERRED;
+    final double gap = 15;
+    double[] cols = {gap, pref, gap, fill, gap, pref, gap};
+    double[] rows = {gap,
+                     pref,
+                     gap,
+                     pref,
+                     gap};
+    setLayout(new TableLayout(cols, rows));
+    TableLayoutUtil tlu = new TableLayoutUtil(this);
+    tlu.add(1, 1, new JLabel(SSH_KEY_FIELD_LABEL));
+    tlu.add(3, 1, sshKeyField);
+    tlu.add(5, 1, _browseSshKeyButton());
+    tlu.add(1, 3, new JLabel(SSH_PASSPHRASE_FIELD_LABEL));
+    tlu.add(3, 3, 5, 3, sshPassphraseField);
+  }
+
+  private JButton _browseSshKeyButton()
+  {
+    JButton locationBrowseButton = new JButton("Browse");
+    locationBrowseButton.addActionListener(e -> {
+      JFileChooser fc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+      fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+      int returnValue = fc.showSaveDialog(null);
+      if (returnValue == JFileChooser.APPROVE_OPTION)
+      {
+        sshKeyField.setText(fc.getSelectedFile().getAbsolutePath());
+      }
+    });
+    return locationBrowseButton;
   }
 
   @Override
