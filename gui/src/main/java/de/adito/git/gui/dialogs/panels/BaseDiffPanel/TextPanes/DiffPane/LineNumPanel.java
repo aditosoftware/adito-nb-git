a94c9420570fc79b8777607ee199cd9daefb9a9b
m.kaspera <m.kaspera@adito.de>
2019-01-18 16:44:43 +0100

    Changed the way the lineNumbers and LineNumberColors to be drawn are determined (optimized for speed, needs more memory now)
    The y coordinates for the LineNumbers/LineNumberColors are only calculated if it is absolutely necessary now, not every time the viewPort changed.
    If the viewPort only moves the objects in the viewPort are only calculated with the pre-determined coordinates

--- gui/src/main/java/de/adito/git/gui/dialogs/panels/BaseDiffPanel/TextPanes/DiffPane/LineNumPanel.java
+++ gui/src/main/java/de/adito/git/gui/dialogs/panels/BaseDiffPanel/TextPanes/DiffPane/LineNumPanel.java
@@ -28,8 +28,12 @@ class LineNumPanel extends JPanel implements IDiscardable
   private static final int LINE_NUM_FACADE_WIDTH = 30;
   private final DiffPanelModel model;
   private final Disposable disposable;
-  private List<LineNumber> lineNumbers = new ArrayList<>();
-  private List<LineNumberColor> lineNumberColors = new ArrayList<>();
+  private Rectangle cachedViewRectangle = new Rectangle();
+  // lists with Objects that contain information about what to draw. Never modify these lists by themselves, only re-assign them
+  private List<LineNumber> lineNumbers = new ArrayList<>(); // all LineNumbers for the file in the editor, irrespective of if they are shown
+  private List<LineNumber> drawnLineNumbers = new ArrayList<>(); // LineNumbers currently in the viewPort, these have to be drawn
+  private List<LineNumberColor> lineNumberColors = new ArrayList<>(); // all LineNumberColors for the file in the editor
+  private List<LineNumberColor> drawnLineNumberColors = new ArrayList<>(); // LineNumberColors currently in the viewPort, these have to be drawn
   private final Insets editorPaneInsets;
 
 
@@ -48,8 +52,8 @@ class LineNumPanel extends JPanel implements IDiscardable
         pModel.getFileChangesObservable(), pDisplayedArea, FileChangesRectanglePair::new)
         .subscribe(
             pPair -> SwingUtilities.invokeLater(() -> {
-              lineNumbers = _calculateLineNumbers(pEditorPane, pPair.getFileChangesEvent(), pPair.getRectangle());
-              lineNumberColors = _calculateLineNumberColors(pEditorPane, pPair.getFileChangesEvent(), pPair.getRectangle());
+              _calculateLineNumbers(pEditorPane, pPair.getFileChangesEvent(), pPair.getRectangle());
+              _calculateLineNumColors(pEditorPane, pPair.getFileChangesEvent(), pPair.getRectangle());
               repaint();
             }));
   }
@@ -60,7 +64,7 @@ class LineNumPanel extends JPanel implements IDiscardable
     super.paintComponent(pGraphics);
     // passing the local variable here, this means we pass a reference to the current list. Combine this with always creating a new, separate list and
     // assigning it and there should be no ConcurrentModificationExceptions
-    _paintLines(pGraphics, lineNumbers, lineNumberColors);
+    _paintLines(pGraphics, drawnLineNumbers, drawnLineNumberColors);
   }
 
   @Override
@@ -69,37 +73,133 @@ class LineNumPanel extends JPanel implements IDiscardable
     disposable.dispose();
   }
 
-
   /**
-   * @param pEditorPane       EditorPane that contains the text of the IFileChangeChunks in pFileChangesEvent
-   * @param pFileChangesEvent IFileChangesEvent that contains the most up-to-date IFileChangeChunks
-   * @param pRectangle        Rectangle with the coordinates of the viewPort
-   * @return List of LineNumberColors that should be drawn
+   * @param pEditorPane       JEditorPane that contains the text for which the line numbers should be drawn
+   * @param pFileChangesEvent most recent IFileChangesEvent
+   * @param pViewWindow       Coordinates of the viewPort window, in view coordinates
    */
-  private List<LineNumberColor> _calculateLineNumberColors(JEditorPane pEditorPane, IFileChangesEvent pFileChangesEvent, Rectangle pRectangle)
+  private void _calculateLineNumbers(@NotNull JEditorPane pEditorPane, IFileChangesEvent pFileChangesEvent, Rectangle pViewWindow)
   {
-    ArrayList<LineNumberColor> lineNumColors = new ArrayList<>();
-    try
+    if (pViewWindow.width != cachedViewRectangle.width || lineNumbers.isEmpty())
     {
-      View view = pEditorPane.getUI().getRootView(pEditorPane);
-      int lineCounter = 0;
-      for (IFileChangeChunk fileChange : pFileChangesEvent.getNewValue())
+      try
       {
-        int numLines = model.getGetEndLine().apply(fileChange) - model.getGetStartLine().apply(fileChange);
-        if (fileChange.getChangeType() != EChangeType.SAME)
+        List<LineNumber> lineNums = new ArrayList<>();
+        View view = pEditorPane.getUI().getRootView(pEditorPane);
+        int lineCounter = 0;
+        int numberedLineCounter = 1;
+        for (IFileChangeChunk fileChange : pFileChangesEvent.getNewValue())
         {
-          LineNumberColor lineNumberColor = _addLineNumColor(pEditorPane, lineCounter, numLines, fileChange, view, pRectangle);
-          if (lineNumberColor != null)
-            lineNumColors.add(lineNumberColor);
+          int numLines = model.getGetEndLine().apply(fileChange) - model.getGetStartLine().apply(fileChange);
+          for (int index = 0; index < numLines; index++)
+          {
+            lineNums.add(_calculateLineNumberPosition(pEditorPane, numberedLineCounter, lineCounter, index, view));
+          }
+          numberedLineCounter += numLines;
+          lineCounter += numLines + model.getGetParityLines().apply(fileChange).length();
         }
-        lineCounter += numLines + model.getGetParityLines().apply(fileChange).length();
+        lineNumbers = lineNums;
+      }
+      catch (BadLocationException pE)
+      {
+        throw new RuntimeException(pE);
       }
     }
-    catch (BadLocationException pE)
+    drawnLineNumbers = _calculateDrawnLineNumbers(lineNumbers, pViewWindow);
+    cachedViewRectangle = pViewWindow;
+  }
+
+  /**
+   * @param pLineNumbers list of all LineNumbers to be filtered
+   * @param pViewWindow  Rectangle with the coordinates of the viewPort
+   * @return list of LineNumbers of the the incoming list that are in the viewPort area
+   */
+  private List<LineNumber> _calculateDrawnLineNumbers(List<LineNumber> pLineNumbers, Rectangle pViewWindow)
+  {
+    List<LineNumber> lineNumbersToDraw = new ArrayList<>();
+    for (LineNumber lineNumber : pLineNumbers)
     {
-      throw new RuntimeException(pE);
+      pViewWindow.intersects(lineNumber.getXCoordinate(), lineNumber.getYCoordinate() - pViewWindow.y, Integer.MAX_VALUE, 16);
+      lineNumbersToDraw.add(new LineNumber(Integer.valueOf(lineNumber.getNumber()), lineNumber.getYCoordinate() - pViewWindow.y,
+                                           lineNumber.getXCoordinate()));
     }
-    return lineNumColors;
+    return lineNumbersToDraw;
+  }
+
+  /**
+   * @param pEditorPane          JEditorPane containing the text for which theses lines are
+   * @param pNumberedLineCounter number of actual lines before this changeChunk
+   * @param pLineCounter         number of total lines (including parity lines) before this changeChunk
+   * @param pIndex               current line inside the changeChunk
+   * @param pView                View for getting the y coordinates for line numbers
+   * @return LineNumber object for the line determined in the input parameters
+   * @throws BadLocationException if the line is out of scope of the document
+   */
+  private LineNumber _calculateLineNumberPosition(JEditorPane pEditorPane, int pNumberedLineCounter,
+                                                  int pLineCounter, int pIndex, View pView) throws BadLocationException
+  {
+    Element lineElement = pEditorPane.getDocument().getDefaultRootElement().getElement(pLineCounter + pIndex);
+    if (lineElement == null)
+      throw new BadLocationException("Element in Document for line was null", pLineCounter + pIndex);
+    int startOffset = lineElement.getStartOffset();
+    int yViewCoordinate = pView.modelToView(startOffset, Position.Bias.Forward, startOffset + 1, Position.Bias.Forward, new Rectangle())
+        .getBounds().y;
+    return new LineNumber(pNumberedLineCounter + pIndex, yViewCoordinate, editorPaneInsets.left + 2);
+  }
+
+  /**
+   * @param pEditorPane       JEditorPane with the text from the IFileChangesEvent. It's UI defines the y values for the LineNumColors
+   * @param pFileChangesEvent currentIFileChangesEvent
+   * @param pViewWindow       Rectangle with coordinates of the current viewPort
+   */
+  private void _calculateLineNumColors(JEditorPane pEditorPane, IFileChangesEvent pFileChangesEvent, Rectangle pViewWindow)
+  {
+    if (pViewWindow.width != cachedViewRectangle.width || lineNumberColors.isEmpty())
+    {
+      try
+      {
+        List<LineNumberColor> lineNumColors = new ArrayList<>();
+        View view = pEditorPane.getUI().getRootView(pEditorPane);
+        int lineCounter = 0;
+        for (IFileChangeChunk fileChange : pFileChangesEvent.getNewValue())
+        {
+          int numLines = model.getGetEndLine().apply(fileChange) - model.getGetStartLine().apply(fileChange);
+          if (fileChange.getChangeType() != EChangeType.SAME)
+          {
+            lineNumColors.add(_viewCoordinatesLineNumberColor(pEditorPane, lineCounter, numLines, fileChange, view));
+          }
+          lineCounter += numLines + model.getGetParityLines().apply(fileChange).length();
+        }
+        lineNumberColors = lineNumColors;
+      }
+      catch (BadLocationException pE)
+      {
+        throw new RuntimeException(pE);
+      }
+    }
+    drawnLineNumberColors = _calculateDrawnLineNumberColors(lineNumberColors, pViewWindow);
+    cachedViewRectangle = pViewWindow;
+  }
+
+  /**
+   * @param pAllLineNumberColors list with all lineNumberColor objects, will be filtered by which are in the viewPort
+   * @param pViewRectangle       Rectangle with coordinates of the current viewPort
+   * @return list with all LineNumberColors that are in the viewPort and have to be drawn
+   */
+  private List<LineNumberColor> _calculateDrawnLineNumberColors(List<LineNumberColor> pAllLineNumberColors, Rectangle pViewRectangle)
+  {
+    List<LineNumberColor> lineNumberColorsToDraw = new ArrayList<>();
+    for (LineNumberColor lineNumberColor : pAllLineNumberColors)
+    {
+      if (pViewRectangle.intersects(lineNumberColor.getColoredArea()))
+      {
+        Rectangle toDrawRect = new Rectangle(lineNumberColor.getColoredArea());
+        toDrawRect.y = toDrawRect.y - pViewRectangle.y;
+        toDrawRect.x = toDrawRect.x + editorPaneInsets.left;
+        lineNumberColorsToDraw.add(new LineNumberColor(lineNumberColor.getColor(), toDrawRect));
+      }
+    }
+    return lineNumberColorsToDraw;
   }
 
   /**
@@ -108,19 +208,18 @@ class LineNumPanel extends JPanel implements IDiscardable
    * @param pNumLines    number of lines that this LineNumColor should encompass
    * @param pFileChange  IFileChangeChunk that is the reason for this LineNumColor
    * @param pView        rootView of the UI of the EditorPane, to determine the location of lines in view coordinates
-   * @param pRectangle   Rectangle with the coordinates of the viewPort
    * @return LineNumberColor with the gathered information about where and what color the LineNumberColor should be drawn
    * @throws BadLocationException i.e. if the line is out of bounds
    */
-  private LineNumberColor _addLineNumColor(JEditorPane pEditorPane, int pLineCounter, int pNumLines,
-                                           IFileChangeChunk pFileChange, View pView, Rectangle pRectangle) throws BadLocationException
+  private LineNumberColor _viewCoordinatesLineNumberColor(JEditorPane pEditorPane, int pLineCounter, int pNumLines, IFileChangeChunk pFileChange,
+                                                          View pView) throws BadLocationException
   {
     Element startingLineElement = pEditorPane.getDocument().getDefaultRootElement().getElement(pLineCounter);
     Element endingLineElement = pEditorPane.getDocument().getDefaultRootElement()
         .getElement(pLineCounter + pNumLines + model.getGetParityLines().apply(pFileChange).length() - 1);
+    Rectangle bounds;
     if (startingLineElement != null && endingLineElement != null)
     {
-      Rectangle bounds;
       // case "insert stuff here", no parity lines and pNumLines was 0 -> endingLineElement is of line before startingLineElement
       if (startingLineElement.getStartOffset() == endingLineElement.getEndOffset())
       {
@@ -136,55 +235,10 @@ class LineNumPanel extends JPanel implements IDiscardable
         bounds = pView.modelToView(startingLineElement.getStartOffset(), Position.Bias.Forward,
                                    endingLineElement.getEndOffset(), Position.Bias.Backward, new Rectangle()).getBounds();
       }
-      if (pRectangle.intersects(bounds))
-      {
-        bounds.y = bounds.y - pRectangle.y + editorPaneInsets.top;
-        bounds.x = bounds.x + editorPaneInsets.left;
-        return new LineNumberColor(pFileChange.getChangeType().getDiffColor(), bounds);
-      }
+      return new LineNumberColor(pFileChange.getChangeType().getDiffColor(), bounds);
     }
-    return null;
-  }
-
-  /**
-   * @param pEditorPane       JEditorPane that contains the text for which the line numbers should be drawn
-   * @param pFileChangesEvent most recent IFileChangesEvent
-   * @param pViewWindow       Coordinates of the viewPort window, in view coordinates
-   */
-  private List<LineNumber> _calculateLineNumbers(@NotNull JEditorPane pEditorPane, IFileChangesEvent pFileChangesEvent, Rectangle pViewWindow)
-  {
-    ArrayList<LineNumber> lineNumberings = new ArrayList<>();
-    try
-    {
-      View view = pEditorPane.getUI().getRootView(pEditorPane);
-      int lineCounter = 0;
-      int numberedLineCounter = 1;
-      for (IFileChangeChunk fileChange : pFileChangesEvent.getNewValue())
-      {
-        int numLines = model.getGetEndLine().apply(fileChange) - model.getGetStartLine().apply(fileChange);
-        for (int index = 0; index < numLines; index++)
-        {
-          Element lineElement = pEditorPane.getDocument().getDefaultRootElement().getElement(lineCounter + index);
-          if (lineElement == null)
-            break;
-          int startOffset = lineElement.getStartOffset();
-          int yViewCoordinate = view.modelToView(startOffset, Position.Bias.Forward, startOffset + 1, Position.Bias.Forward, new Rectangle())
-              .getBounds().y;
-          int yVisibleCoordinate = yViewCoordinate - pViewWindow.y;
-          Rectangle numberBounds = new Rectangle(editorPaneInsets.left + 2, yViewCoordinate, Integer.MAX_VALUE, 16);
-          if (pViewWindow.intersects(numberBounds))
-            lineNumberings.add(new LineNumber(numberedLineCounter + index,
-                                              yVisibleCoordinate, numberBounds.x));
-        }
-        numberedLineCounter += numLines;
-        lineCounter += numLines + model.getGetParityLines().apply(fileChange).length();
-      }
-    }
-    catch (BadLocationException pE)
-    {
-      throw new RuntimeException(pE);
-    }
-    return lineNumberings;
+    throw new BadLocationException("could not find Element for provided lines", startingLineElement == null ? pLineCounter :
+        pLineCounter + pNumLines + model.getGetParityLines().apply(pFileChange).length() - 1);
   }
 
   /**
@@ -203,10 +257,10 @@ class LineNumPanel extends JPanel implements IDiscardable
       pGraphics.fillRect(lineNumberColor.getColoredArea().x, lineNumberColor.getColoredArea().y,
                          lineNumberColor.getColoredArea().width, lineNumberColor.getColoredArea().height);
     }
+    ((Graphics2D) pGraphics).setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+    pGraphics.setColor(new Color(0xff888888, true));
     for (LineNumber lineNumber : pLineNumbers)
     {
-      ((Graphics2D) pGraphics).setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-      pGraphics.setColor(new Color(0xff888888, true));
       pGraphics.drawString(lineNumber.getNumber(), lineNumber.getXCoordinate(),
                            lineNumber.getYCoordinate() + pGraphics.getFontMetrics().getHeight());
     }
