ca244ee60aa8deb9684d3328250287b23bcfed5d
m.kaspera <m.kaspera@adito.de>
2019-02-08 16:00:50 +0100

    Changed colors to a more suitable color scheme, also went up from 12 different colors to 20. Also fixed indentation

--- api/src/main/java/de/adito/git/api/ColorRoulette.java
+++ api/src/main/java/de/adito/git/api/ColorRoulette.java
@@ -3,66 +3,79 @@ package de.adito.git.api;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.Color;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 /**
  * Queue-backed list of colors that delivers colors/takes them back. Operates on FIFO principle
  *
  * @author m.kaspera 20.11.2018
  */
-public class ColorRoulette {
+public class ColorRoulette
+{
 
-    ColorRoulette() {
-        availableColors.addAll(initialColors);
+  private final List<Color> initialColors = Arrays.asList(
+      new Color(101, 179, 46),
+      new Color(241, 200, 23),
+      new Color(229, 28, 40),
+      new Color(45, 53, 140),
+      new Color(18, 146, 69),
+      new Color(4, 112, 184),
+      new Color(243, 146, 32),
+      new Color(153, 133, 117),
+      new Color(158, 19, 95),
+      new Color(250, 250, 60),
+      new Color(49, 174, 114),
+      new Color(117, 77, 36),
+      new Color(30, 171, 227),
+      new Color(235, 91, 38),
+      new Color(13, 104, 56),
+      new Color(73, 73, 73),
+      new Color(39, 40, 91),
+      new Color(215, 219, 45),
+      new Color(193, 39, 47),
+      new Color(231, 230, 230)
+  );
+  private final LinkedList<Color> availableColors = new LinkedList<>();
+
+  ColorRoulette()
+  {
+    availableColors.addAll(initialColors);
+  }
+
+  /**
+   * @return the next Color in the queue or null if an error occurs/the queue is empty
+   */
+  @NotNull
+  public Color get()
+  {
+    try
+    {
+      return availableColors.remove();
     }
-
-    private final List<Color> initialColors = Arrays.asList(
-            new Color(0, 255, 0),
-            new Color(0, 255, 255),
-            new Color(255, 255, 0),
-            new Color(200, 200, 200),
-            new Color(255, 0, 0),
-            new Color(0, 0, 255),
-            new Color(200, 100, 0),
-            new Color(150, 200, 200),
-            new Color(200, 200, 0),
-            new Color(0, 200, 200),
-            new Color(200, 100, 100),
-            new Color(0, 100, 200),
-            new Color(100, 255, 255)
-    );
-    private final LinkedList<Color> availableColors = new LinkedList<>();
-
-    /**
-     * @return the next Color in the queue or null if an error occurs/the queue is empty
-     */
-    @NotNull
-    public Color get() {
-        try {
-            return availableColors.remove();
-        } catch (NoSuchElementException e) {
-            return Color.red;
-        }
+    catch (NoSuchElementException e)
+    {
+      return Color.red;
     }
+  }
 
-    /**
-     * @param color the Color that should be returned to the queue
-     */
-    void returnColor(@NotNull Color color) {
-        if (!availableColors.contains(color)) {
-            availableColors.add(color);
-        }
+  /**
+   * @param pColor the Color that should be returned to the queue
+   */
+  void returnColor(@NotNull Color pColor)
+  {
+    if (!availableColors.contains(pColor))
+    {
+      availableColors.add(pColor);
     }
+  }
 
-    /**
-     * resets the contents of the Queue back to its original state
-     */
-    public void reset() {
-        availableColors.clear();
-        availableColors.addAll(initialColors);
-    }
+  /**
+   * resets the contents of the Queue back to its original state
+   */
+  public void reset()
+  {
+    availableColors.clear();
+    availableColors.addAll(initialColors);
+  }
 
 }
