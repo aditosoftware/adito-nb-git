2b74687a40e10b2461d52d2cd129b677e2d77c58
m.kaspera <m.kaspera@adito.de>
2019-03-12 10:50:26 +0100

    Diff Action is only enabled if there is a file that is actually selected

--- gui/src/main/java/de/adito/git/gui/actions/DiffToHeadAction.java
+++ gui/src/main/java/de/adito/git/gui/actions/DiffToHeadAction.java
@@ -3,17 +3,22 @@ package de.adito.git.gui.actions;
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
 import de.adito.git.api.IRepository;
-import de.adito.git.api.data.*;
+import de.adito.git.api.data.EChangeSide;
+import de.adito.git.api.data.IFileChangeType;
+import de.adito.git.api.data.IFileDiff;
 import de.adito.git.api.progress.IAsyncProgressFacade;
 import de.adito.git.gui.Constants;
-import de.adito.git.gui.dialogs.*;
+import de.adito.git.gui.dialogs.DialogResult;
+import de.adito.git.gui.dialogs.IDialogProvider;
 import de.adito.git.gui.icon.IIconLoader;
 import io.reactivex.Observable;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.io.*;
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -32,7 +37,7 @@ class DiffToHeadAction extends AbstractTableAction
                    @Assisted Observable<Optional<IRepository>> pRepository,
                    @Assisted Observable<Optional<List<IFileChangeType>>> pSelectedFilesObservable)
   {
-    super("Compare with local");
+    super("Compare with local", _getIsEnabledObservable(pSelectedFilesObservable));
     putValue(Action.SMALL_ICON, pIconLoader.getIcon(Constants.DIFF_ACTION_ICON));
     progressFacade = pProgressFacade;
     repository = pRepository;
@@ -83,6 +88,11 @@ class DiffToHeadAction extends AbstractTableAction
     {
       throw new RuntimeException(pE);
     }
+  }
+
+  private static Observable<Optional<Boolean>> _getIsEnabledObservable(Observable<Optional<List<IFileChangeType>>> pSelectedFilesObservable)
+  {
+    return pSelectedFilesObservable.map(pSelectedFilesOpt -> pSelectedFilesOpt.map(pSelectedFiles -> !pSelectedFiles.isEmpty()));
   }
 
 }
