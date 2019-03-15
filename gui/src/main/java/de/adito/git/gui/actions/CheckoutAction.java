3138c069da27d9d398884718effaaa5e4f55df21
m.kaspera <m.kaspera@adito.de>
2019-03-15 18:23:26 +0100

    Added stashing before checkout if local changes exist, will be un-stashed after the checkout

--- gui/src/main/java/de/adito/git/gui/actions/CheckoutAction.java
+++ gui/src/main/java/de/adito/git/gui/actions/CheckoutAction.java
@@ -4,9 +4,12 @@ import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
 import de.adito.git.api.IRepository;
 import de.adito.git.api.data.IBranch;
+import de.adito.git.api.data.IFileStatus;
+import de.adito.git.api.prefs.IPrefStore;
 import de.adito.git.api.progress.IAsyncProgressFacade;
+import de.adito.git.gui.actions.commands.StashCommand;
+import de.adito.git.gui.dialogs.IDialogProvider;
 import io.reactivex.Observable;
-import io.reactivex.subjects.BehaviorSubject;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -19,35 +22,65 @@ import java.util.Optional;
  */
 class CheckoutAction extends AbstractTableAction
 {
+  private static final String STASH_ID_KEY = "checkout::stashCommitId";
+  private final IPrefStore prefStore;
+  private final IDialogProvider dialogProvider;
   private final IAsyncProgressFacade progressFactory;
-  private Observable<Optional<IRepository>> repository;
-  private Observable<Optional<IBranch>> branch;
+  private Observable<Optional<IRepository>> repositoryObservable;
+  private Observable<Optional<IBranch>> branchObservable;
 
   /**
    * @param pRepository The repository where the branch is
    * @param pBranch     the branch list of selected branches
    */
   @Inject
-  CheckoutAction(IAsyncProgressFacade pProgressFactory, @Assisted Observable<Optional<IRepository>> pRepository,
-                 @Assisted Observable<Optional<IBranch>> pBranch)
+  CheckoutAction(IPrefStore pPrefStore, IDialogProvider pDialogProvider, IAsyncProgressFacade pProgressFactory,
+                 @Assisted Observable<Optional<IRepository>> pRepository, @Assisted Observable<Optional<IBranch>> pBranch)
   {
-    super("Checkout");
+    super("Checkout", _getIsEnabledObservable());
+    prefStore = pPrefStore;
+    dialogProvider = pDialogProvider;
     progressFactory = pProgressFactory;
-    branch = pBranch;
+    branchObservable = pBranch;
     putValue(Action.NAME, "Checkout");
     putValue(Action.SHORT_DESCRIPTION, "Command to change the branch to another one");
-    repository = pRepository;
+    repositoryObservable = pRepository;
   }
 
   @Override
-  public void actionPerformed(ActionEvent e)
+  public void actionPerformed(ActionEvent pEvent)
   {
-    Optional<IBranch> branchOpt = branch.blockingFirst();
+    IRepository repository = repositoryObservable.blockingFirst().orElseThrow(() -> new RuntimeException("no valid repository found"));
+    Optional<IBranch> branchOpt = branchObservable.blockingFirst();
     if (branchOpt.isPresent())
     {
       IBranch branch = branchOpt.get();
       progressFactory.executeInBackground("Checking out " + branch.getSimpleName(), pProgress -> {
-        repository.blockingFirst().orElseThrow(() -> new RuntimeException("no valid repository found")).checkout(branch);
+        try
+        {
+          if (repository.getStatus().blockingFirst().map(IFileStatus::hasUncommittedChanges).orElse(false))
+          {
+            pProgress.setDescription("Stashing uncommitted local changes");
+            prefStore.put(STASH_ID_KEY, repository.stashChanges(null, false));
+          }
+          repository.checkout(branch);
+        }
+        finally
+        {
+          pProgress.setDescription("Un-stashing saved uncommitted local changes");
+          String stashedCommitId = prefStore.get(STASH_ID_KEY);
+          if (stashedCommitId != null)
+          {
+            try
+            {
+              StashCommand.doUnStashing(dialogProvider, stashedCommitId, Observable.just(Optional.of(repository)));
+            }
+            finally
+            {
+              prefStore.put(STASH_ID_KEY, null);
+            }
+          }
+        }
       });
     }
   }
@@ -57,7 +90,7 @@ class CheckoutAction extends AbstractTableAction
    *
    * @return return true if the selected list has one element, else false
    */
-  private static Observable<Optional<Boolean>> getIsEnabledObservable()
+  private static Observable<Optional<Boolean>> _getIsEnabledObservable()
   {
     return Observable.just(Optional.of(true));
   }
