66dca926baecf8eb82d8ffe7c99ef5d5af016601
m.kaspera <m.kaspera@adito.de>
2019-05-28 09:27:12 +0200

    Changed the order in which the colorRoulette colors were arranged to a more continuous color gradient

--- api/src/main/java/de/adito/git/api/ColorRoulette.java
+++ api/src/main/java/de/adito/git/api/ColorRoulette.java
@@ -15,31 +15,39 @@ public class ColorRoulette
 
   private final List<Color> initialColors = Arrays.asList(
       new Color(101, 179, 46),
-      new Color(241, 200, 23),
-      new Color(229, 28, 40),
-      new Color(45, 53, 140),
-      new Color(18, 146, 69),
-      new Color(4, 112, 184),
-      new Color(243, 146, 32),
-      new Color(153, 133, 117),
-      new Color(158, 19, 95),
-      new Color(250, 250, 60),
       new Color(49, 174, 114),
-      new Color(117, 77, 36),
-      new Color(30, 171, 227),
-      new Color(235, 91, 38),
+      new Color(18, 146, 69),
       new Color(13, 104, 56),
+      new Color(153, 133, 117),
       new Color(141, 129, 188),
-      new Color(39, 40, 91),
+      new Color(241, 200, 23),
+      new Color(250, 250, 60),
       new Color(215, 219, 45),
+      new Color(243, 146, 32),
+      new Color(235, 91, 38),
+      new Color(229, 28, 40),
       new Color(193, 39, 47),
+      new Color(158, 19, 95),
+      new Color(39, 40, 91),
+      new Color(45, 53, 140),
+      new Color(4, 112, 184),
+      new Color(30, 171, 227),
+      new Color(117, 77, 36),
       new Color(231, 230, 230)
   );
   private final LinkedList<Color> availableColors = new LinkedList<>();
 
-  ColorRoulette()
+  private ColorRoulette()
   {
     availableColors.addAll(initialColors);
+  }
+
+  /**
+   * @return a new ColorRoulette instance
+   */
+  public static ColorRoulette create()
+  {
+    return new ColorRoulette();
   }
 
   /**
@@ -59,9 +67,11 @@ public class ColorRoulette
   }
 
   /**
+   * puts the color into the queue of available colors
+   *
    * @param pColor the Color that should be returned to the queue
    */
-  void returnColor(@NotNull Color pColor)
+  public void returnColor(@NotNull Color pColor)
   {
     if (!availableColors.contains(pColor))
     {
