28a7144a1042701487de136aa8fc8a205f005301
m.haertel <m.haertel@adito.de>
2019-08-07 17:28:13 +0200

    Renaming of iconBase Constants: The suffix "_dark" for icons is no longer needed. The IconLoader and the LookAndFeel take care for choosing the correct Icon

--- gui/src/main/java/de/adito/git/gui/Constants.java
+++ gui/src/main/java/de/adito/git/gui/Constants.java
@@ -11,36 +11,36 @@ public class Constants
   public static final int SCROLL_SPEED_INCREMENT = 16;
 
   // Merge Dialog Icons
-  public static final String ACCEPT_CHANGE_YOURS_ICON = "/de/adito/git/gui/icons/acceptChange_dark.png";
-  public static final String ACCEPT_CHANGE_THEIRS_ICON = "/de/adito/git/gui/icons/acceptChangeRight_dark.png";
-  public static final String DISCARD_CHANGE_ICON = "/de/adito/git/gui/icons/discardChange_dark.png";
+  public static final String ACCEPT_CHANGE_YOURS_ICON = "/de/adito/git/gui/icons/acceptChange.png";
+  public static final String ACCEPT_CHANGE_THEIRS_ICON = "/de/adito/git/gui/icons/acceptChangeRight.png";
+  public static final String DISCARD_CHANGE_ICON = "/de/adito/git/gui/icons/discardChange.png";
   public static final String ARROW_RIGHT = "/de/adito/git/gui/icons/arrowRight.png";
 
   // Diff Dialog Icons
-  public static final String NEXT_OCCURRENCE = "/de/adito/git/gui/icons/next_dark.png";
-  public static final String PREVIOUS_OCCURRENCE = "/de/adito/git/gui/icons/previous_dark.png";
+  public static final String NEXT_OCCURRENCE = "/de/adito/git/gui/icons/next.png";
+  public static final String PREVIOUS_OCCURRENCE = "/de/adito/git/gui/icons/previous.png";
 
   // Action Icons
   public static final String COMMIT_ACTION_ICON = "/de/adito/git/nbm/actions/commit.png";
-  public static final String REVERT_ACTION_ICON = "/de/adito/git/nbm/actions/revert_dark.png";
-  public static final String DIFF_ACTION_ICON = "/de/adito/git/nbm/actions/diff_dark.png";
-  public static final String FILE_HISTORY_ACTION_ICON = "/de/adito/git/gui/icons/file_history_dark.png";
-  public static final String SHOW_TAGS_ACTION_ICON = "/de/adito/git/gui/icons/tags_dark.png";
-  public static final String GIT_CONFIG_ICON = "/de/adito/git/gui/icons/settings_dark.png";
-  public static final String REFRESH_CONTENT_ICON = "/de/adito/git/gui/icons/refresh_dark.png";
-  public static final String DELETE_ICON = "/de/adito/git/gui/icons/delete_dark.png";
-  public static final String ADD_TAG_ACTION_ICON = "/de/adito/git/gui/icons/tag_icon_dark.png";
-  public static final String EXPAND_TREE_ACTION_ICON = "/de/adito/git/gui/icons/expand_dark.png";
-  public static final String COLLAPSE_TREE_ACTION_ICON = "/de/adito/git/gui/icons/compress_dark.png";
-  public static final String CHERRY_PICK = "/de/adito/git/gui/icons/cherrypick_dark.png";
-  public static final String ACCEPT_ALL_LEFT = "/de/adito/git/gui/icons/acceptLeft_dark.png";
-  public static final String ACCEPT_ALL_RIGHT = "/de/adito/git/gui/icons/acceptRight_dark.png";
-  public static final String ACCEPT_ALL_NON_CONFLICTING = "/de/adito/git/gui/icons/accept-non-conflicting_dark.png";
-  public static final String SWITCH_TREE_VIEW_FLAT = "/de/adito/git/gui/icons/tree_view_flat_dark.png";
-  public static final String SWITCH_TREE_VIEW_HIERARCHICAL = "/de/adito/git/gui/icons/tree_view_hierarchical_dark.png";
+  public static final String REVERT_ACTION_ICON = "/de/adito/git/nbm/actions/revert.png";
+  public static final String DIFF_ACTION_ICON = "/de/adito/git/nbm/actions/diff.png";
+  public static final String FILE_HISTORY_ACTION_ICON = "/de/adito/git/gui/icons/file_history.png";
+  public static final String SHOW_TAGS_ACTION_ICON = "/de/adito/git/gui/icons/tags.png";
+  public static final String GIT_CONFIG_ICON = "/de/adito/git/gui/icons/settings.png";
+  public static final String REFRESH_CONTENT_ICON = "/de/adito/git/gui/icons/refresh.png";
+  public static final String DELETE_ICON = "/de/adito/git/gui/icons/delete.png";
+  public static final String ADD_TAG_ACTION_ICON = "/de/adito/git/gui/icons/tag_icon.png";
+  public static final String EXPAND_TREE_ACTION_ICON = "/de/adito/git/gui/icons/expand.png";
+  public static final String COLLAPSE_TREE_ACTION_ICON = "/de/adito/git/gui/icons/compress.png";
+  public static final String CHERRY_PICK = "/de/adito/git/gui/icons/cherrypick.png";
+  public static final String ACCEPT_ALL_LEFT = "/de/adito/git/gui/icons/acceptLeft.png";
+  public static final String ACCEPT_ALL_RIGHT = "/de/adito/git/gui/icons/acceptRight.png";
+  public static final String ACCEPT_ALL_NON_CONFLICTING = "/de/adito/git/gui/icons/accept-non-conflicting.png";
+  public static final String SWITCH_TREE_VIEW_FLAT = "/de/adito/git/gui/icons/tree_view_flat.png";
+  public static final String SWITCH_TREE_VIEW_HIERARCHICAL = "/de/adito/git/gui/icons/tree_view_hierarchical.png";
 
   // Sidebar Icon
-  public static final String ROLLBACK_ICON = "/de/adito/git/nbm/sidebar/rollback_dark.png";
+  public static final String ROLLBACK_ICON = "/de/adito/git/nbm/sidebar/rollback.png";
 
   // Branch Icon in CommitHistoryWindow
   public static final String BRANCH_ICON_LOCAL = "/de/adito/git/gui/icons/branch_icon_local.png";
