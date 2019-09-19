b9b6e485ee4fcd28d6d0b8e6f558663ef11ae495
m.kaspera <m.kaspera@adito.de>
2019-09-19 07:39:44 +0200

    The config and gitignore files can now be opened from the settings menu

--- gui/src/main/java/de/adito/git/gui/dialogs/GitConfigDialog.java
+++ gui/src/main/java/de/adito/git/gui/dialogs/GitConfigDialog.java
@@ -7,11 +7,16 @@ import com.google.inject.assistedinject.Assisted;
 import de.adito.git.api.IDiscardable;
 import de.adito.git.api.IKeyStore;
 import de.adito.git.api.IRepository;
+import de.adito.git.api.data.EChangeType;
+import de.adito.git.api.data.IFileChangeType;
 import de.adito.git.gui.Constants;
+import de.adito.git.gui.actions.IActionProvider;
 import de.adito.git.gui.dialogs.panels.NewRemotePanel;
 import de.adito.git.gui.dialogs.panels.RemotePanel;
+import de.adito.git.impl.data.FileChangeTypeImpl;
 import io.reactivex.Observable;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import javax.swing.border.Border;
@@ -19,6 +24,8 @@ import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.io.File;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.function.Consumer;
 import java.util.logging.Level;
@@ -31,12 +38,13 @@ public class GitConfigDialog extends AditoBaseDialog<Multimap<String, Object>> i
 {
 
   private static final Border DEFAULT_MARGIN_BORDER = new EmptyBorder(15, 15, 0, 0);
+  private static final Border DEFAULT_PANEL_BORDERS = new EmptyBorder(15, 15, 0, 15);
   private NewRemotePanel addRemotePanel;
   private List<RemotePanel> remoteSettingsPanels = new ArrayList<>();
   private GlobalSettingsPanel globalSettingsPanel;
 
   @Inject
-  public GitConfigDialog(IKeyStore pKeyStore, @Assisted Observable<Optional<IRepository>> pRepository)
+  public GitConfigDialog(IActionProvider pActionProvider, IKeyStore pKeyStore, @Assisted Observable<Optional<IRepository>> pRepository)
   {
     setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
@@ -71,7 +79,14 @@ public class GitConfigDialog extends AditoBaseDialog<Multimap<String, Object>> i
       tabbedPane.setSelectedIndex(0);
     }
     globalSettingsPanel = new GlobalSettingsPanel();
+    add(Box.createVerticalStrut(15));
+    JSeparator separator1 = new JSeparator();
+    add(separator1);
     add(globalSettingsPanel);
+    add(Box.createVerticalStrut(15));
+    JSeparator separator2 = new JSeparator();
+    add(separator2);
+    add(new MiscSettingPanel(pActionProvider, pRepository));
   }
 
   @Override
@@ -121,7 +136,7 @@ public class GitConfigDialog extends AditoBaseDialog<Multimap<String, Object>> i
 
     GlobalSettingsPanel()
     {
-      setBorder(new EmptyBorder(15, 15, 0, 15));
+      setBorder(DEFAULT_PANEL_BORDERS);
       setLayout(new BorderLayout(0, 15));
       JLabel titleLabel = _getBoldLabel("Global settings");
       add(titleLabel, BorderLayout.NORTH);
@@ -141,6 +156,53 @@ public class GitConfigDialog extends AditoBaseDialog<Multimap<String, Object>> i
       settingsMap.put(Constants.LOG_LEVEL_SETTINGS_KEY, logLevelBox.getSelectedItem());
       return settingsMap;
     }
+  }
 
+  /**
+   * Contains miscellaneous options/actions that have to do with the settings of a git Repository
+   */
+  private static class MiscSettingPanel extends JPanel
+  {
+
+    MiscSettingPanel(IActionProvider pActionProvider, Observable<Optional<IRepository>> pRepository)
+    {
+      setBorder(DEFAULT_PANEL_BORDERS);
+      setLayout(new BorderLayout(0, 15));
+      JLabel titleLabel = _getBoldLabel("Misc");
+      add(titleLabel, BorderLayout.NORTH);
+      IFileChangeType ignoreFile = _getIgnoreFile(pRepository.blockingFirst().orElse(null));
+      // only show the "open xxx file" if that file actually exists
+      if (ignoreFile != null && ignoreFile.getFile().exists())
+      {
+        Action openFileAction = pActionProvider.getOpenFileAction(Observable.just(Optional.of(List.of(ignoreFile))));
+        openFileAction.putValue(Action.NAME, "Open git ignore file");
+        add(new JButton(openFileAction), BorderLayout.EAST);
+      }
+      IFileChangeType configFile = _getConfigFile(pRepository.blockingFirst().orElse(null));
+      if (configFile != null && configFile.getFile().exists())
+      {
+        Action openConfigFileAction = pActionProvider.getOpenFileAction(Observable.just(Optional.of(List.of(configFile))));
+        openConfigFileAction.putValue(Action.NAME, "Open git config file");
+        add(new JButton(openConfigFileAction), BorderLayout.WEST);
+      }
+    }
+
+    @Nullable
+    private IFileChangeType _getIgnoreFile(@Nullable IRepository pRepo)
+    {
+      if (pRepo == null)
+        return null;
+      File ignoreFileLoc = new File(pRepo.getTopLevelDirectory(), ".gitignore");
+      return new FileChangeTypeImpl(ignoreFileLoc, ignoreFileLoc, EChangeType.SAME);
+    }
+
+    @Nullable
+    private IFileChangeType _getConfigFile(@Nullable IRepository pRepo)
+    {
+      if (pRepo == null)
+        return null;
+      File configFileLoc = Paths.get(pRepo.getTopLevelDirectory().getAbsolutePath(), ".git", "config").toFile();
+      return new FileChangeTypeImpl(configFileLoc, configFileLoc, EChangeType.SAME);
+    }
   }
 }
