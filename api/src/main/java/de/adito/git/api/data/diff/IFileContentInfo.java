8b362e0ae5e58289c614482871006368ffc646a1
m.kaspera <m.kaspera@adito.de>
2020-02-27 15:13:51 +0100

    Added a note that the implementations of the methods in IFileContentInfo should be backed by a cache

--- api/src/main/java/de/adito/git/api/data/diff/IFileContentInfo.java
+++ api/src/main/java/de/adito/git/api/data/diff/IFileContentInfo.java
@@ -12,16 +12,22 @@ public interface IFileContentInfo
 {
 
   /**
+   * Gets the content of the file. Should use a cache of some kind to not always read from the file
+   *
    * @return Supplier of a String with the contents of a file
    */
   Supplier<String> getFileContent();
 
   /**
+   * Get the lineendings used in the file. Should use a cache of some kind to not always read from the file
+   *
    * @return Supplier of the used line endings for the text contents of the file
    */
   Supplier<ELineEnding> getLineEnding();
 
   /**
+   * Get the encoding of the file. Should use a cache of some kind to not always read from the file
+   *
    * @return Supplier of the charset used to create the String from a byte array and vice versa
    */
   Supplier<Charset> getEncoding();
